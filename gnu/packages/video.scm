1ebd25c8512ddc64afc8d77dc229566c2e06f02d
Jesse Gibbons <jgibbons2357@gmail.com>
2020-07-21 00:08:46 -0600

    gnu: ffmpeg-7: Add support for libgme.
    * gnu/packages/video.scm (ffmpeg-7) [inputs]: Add libgme.
    [configure-flags]: Add "--enable-libgme".
    
    Change-Id: I7af4c2cc3af78a808f3d9b8f8e485d666692701a
    Signed-off-by: Maxim Cournoyer <maxim@guixotic.coop>

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -1671,6 +1671,7 @@ operate properly.")
             libcaca
             libcdio-paranoia
             libdrm
+            libgme
             libplacebo
             libtheora
             libva
@@ -1718,7 +1719,6 @@ operate properly.")
       ;;   --enable-libfdk-aac      enable AAC de/encoding via libfdk-aac [no]
       ;;   --enable-libflite        enable flite (voice synthesis) support via
       ;;                            libflite [no]
-      ;;   --enable-libgme          enable Game Music Emu via libgme [no]
       ;;   --enable-libgsm          enable GSM de/encoding via libgsm [no]
       ;;   --enable-libiec61883     enable iec61883 via libiec61883 [no]
       ;;   --enable-libilbc         enable iLBC de/encoding via libilbc [no]
@@ -1771,6 +1771,7 @@ operate properly.")
          "--enable-libcdio"
          "--enable-libdav1d"
          "--enable-libfreetype"
+         "--enable-libgme"
          "--enable-libmp3lame"
          "--enable-libopus"
          "--enable-libplacebo"
