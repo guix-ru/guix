03974494cce0007be42db7359545eca213701abe
Efraim Flashner <efraim@flashner.co.il>
2024-09-25 09:11:53 +0300

    gnu: Add commencement-build-target.
    * gnu/packages/commencement.scm (commencement-build-target): New
    procedure.
    
    Change-Id: Ie479ccd0afd659e843b45cb5cf22717f34ddff5c

--- gnu/packages/commencement.scm
+++ gnu/packages/commencement.scm
@@ -145,6 +145,14 @@ the checkout from TARBALL, a tarball containing said checkout.
              #:graft? #f
              #:local-build? #t))))))
 
+(define* (commencement-build-target #:optional (system (%current-system)))
+  "Return an architecture triplet for use in various build scripts.  This is
+the supported 32-bit version of the architecture currently being built on."
+  (cond
+    ((target-x86-64?) "i686-unknown-linux-gnu")
+    ((target-arm?) "arm-unknown-linux-gnu")
+    (#t (nix-system->gnu-triplet system))))
+
 (define bootar
   (package
     (name "bootar")
