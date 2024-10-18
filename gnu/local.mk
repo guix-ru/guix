ea9119e44a450011ff342f23482c445ce8d27a9e
Janneke Nieuwenhuizen <janneke@gnu.org>
2024-10-18 17:55:27 +0200

    DRAFT installer: Support dry-run via Guile: Add indirection.
    Maybe an extra indirection avoids the need to autoload (most
    of) (newt), (parted), (webutils)?
    
    * gnu/installer.scm (installer-steps-gexp): Rename to...
    (installer-steps): ...this.
    (not-config?, build-compiled-file, apply-locale,
    compute-locale-step, installer-steps): Move to...
    * gnu/installer/compute.scm: ..this new file, autoloaded file.
    * gnu/local.mk (INSTALLER_MODULES): Add it.
    
    Change-Id: Ib566bedc5aaa4713d69fff0b5924b265e0d4d5d3

--- gnu/local.mk
+++ gnu/local.mk
@@ -857,6 +857,7 @@ GNU_SYSTEM_MODULES =				\
 
 INSTALLER_MODULES =                             \
   %D%/installer.scm      			\
+  %D%/installer/compute.scm			\
   %D%/installer/connman.scm			\
   %D%/installer/dump.scm			\
   %D%/installer/final.scm			\
