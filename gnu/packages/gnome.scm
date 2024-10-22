adba2a06c37392d7dc314db82db52d743d3def32
Greg Hogan <code@greghogan.com>
2024-10-22 18:09:55 +0000

    gnu: xffm+: Pin CMake dependency.
    * gnu/packages/gnome.scm (xffm+)[native-inputs]: Change cmake to
    cmake-minimal.
    
    Change-Id: I0cf4fc3bdaa29674b7b9814df94bd12f8303d6bb

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -14209,7 +14209,7 @@ historical battery usage and related statistics.")
               ;; This is done so we can override.
               (("`set.PREFIX_BIN") "set(QPREFIX_BIN")))))))
     (native-inputs
-     (list cmake pkg-config intltool gnu-gettext))
+     (list cmake-minimal pkg-config intltool gnu-gettext))
     (inputs
      (list glib gtk+ libx11 libsm libxv libxaw libxcb libxkbfile
            shared-mime-info))
