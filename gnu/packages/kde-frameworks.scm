cf45b0ead743f99e024125819144b6f7a7ef1f32
Greg Hogan <code@greghogan.com>
2024-10-22 18:09:12 +0000

    gnu: Disable parallel tests.
    * gnu/packages/aidc.scm (zint),
    * gnu/packages/calendar.scm (libical),
    * gnu/packages/compression.scm (c-blosc2, minizip-ng),
    * gnu/packages/cpp.scm (cpp-httplib),
    * gnu/packages/engineering.scm (libmedfile),
    * gnu/packages/gnome.scm (evolution-data-server),
    * gnu/packages/image-processing.scm (opencv),
    * gnu/packages/kde-frameworks.scm (kservice),
    * gnu/packages/maths.scm (scotch),
    * gnu/packages/networking.scm (srt),
    * gnu/packages/tbb.scm (tbb),
    * gnu/packages/tls.scm (mbedtls-lts),
    * gnu/packages/vulkan.scm (vulkan-loader),
    * gnu/packages/web.scm (websocketpp)
    [arguments]<#:parallel-tests?>: Disable.
    
    Change-Id: I88334deb4afc29ab84b279e9d1759a777ddd49dd

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -4531,6 +4531,7 @@ typed.")
      (list kcrash kdbusaddons kdoctools ki18n qtbase qtdeclarative))
     (arguments
      (list
+      #:parallel-tests? #f
       #:phases
       #~(modify-phases %standard-phases
           (add-after 'unpack 'patch
