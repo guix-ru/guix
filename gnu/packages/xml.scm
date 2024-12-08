c8795554ab4d92f8d7507de89daf7383a36d42f5
Janneke Nieuwenhuizen <janneke@gnu.org>
2024-12-08 11:52:26 +0100

    gnu: xmlto: Fix build with gcc-14.
    * gnu/packages/xml.scm (xmlto)[arguments]: Add CFLAGS to #:configure-flags
    relaxing gcc-14's strictness.
    
    Change-Id: I4eb5c18dede03f197aebb6d99c61e1cee307b471

--- gnu/packages/xml.scm
+++ gnu/packages/xml.scm
@@ -1158,7 +1158,8 @@ code for classes that correspond to data structures defined by XMLSchema.")
      ;; Make sure the reference to util-linux's 'getopt' is kept in 'xmlto'.
      (list
       #:configure-flags
-      #~(list (string-append "GETOPT="
+      #~(list "CFLAGS=-g -O2 -Wno-implicit-int"
+              (string-append "GETOPT="
                              #$(this-package-input "util-linux")
                              "/bin/getopt"))))
     (native-inputs
