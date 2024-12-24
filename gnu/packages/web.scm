d4708d3a50b4705b5aa092080f130ca96172f668
Sharlatan Hellseher <sharlatanus@gmail.com>
2024-12-24 16:33:30 +0000

    gnu: go-github-com-mikefarah-yq-v4: Fix bulid.
    * gnu/packages/web.scm (go-github-com-mikefarah-yq-v4, yq) [arguments]
    <test-subdirs>: Limit to project's root.
    
    Change-Id: I12dd2b2c8a0fc11827e4cf5652bb6a614edf7347

--- gnu/packages/web.scm
+++ gnu/packages/web.scm
@@ -5550,6 +5550,7 @@ you'd expect.")
     (build-system go-build-system)
     (arguments
      (list #:import-path "github.com/mikefarah/yq/v4"
+           #:test-subdirs #~(list ".") ; XXX: try to enable all tests
            #:phases
            #~(modify-phases %standard-phases
                (add-after 'install 'remove-binary
@@ -5592,6 +5593,7 @@ JSON, XML, properties, CSV and TSV.")
     (arguments
      (list #:install-source? #f
            #:import-path "github.com/mikefarah/yq/v4"
+           #:test-subdirs #~(list ".") ; XXX: try to enable all tests
            #:phases
            #~(modify-phases %standard-phases
                (add-after 'install 'rename-binary
