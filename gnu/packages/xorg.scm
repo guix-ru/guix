06ecad065e12333c1e12abac76e2a60dbc2f922e
宋文武 <iyzsong@member.fsf.org>
2024-12-28 17:54:26 +0800

    gnu: xev: Update to 1.2.6.
    * gnu/packages/xorg.scm (xev): Update to 1.2.6.
    
    Change-Id: Id9417ea0212f0b41bce04d346278e21965184ed1

--- gnu/packages/xorg.scm
+++ gnu/packages/xorg.scm
@@ -2431,14 +2431,14 @@ DRI (Direct Rendering Infrastructure) drivers.")
 (define-public xev
   (package
     (name "xev")
-    (version "1.2.5")
+    (version "1.2.6")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://xorg/individual/app/xev-"
                            version ".tar.xz"))
        (sha256
-        (base32 "1hbfwcnbyz4w13fbhnghl0vdhf6w9f9pb7jgjwrhykkii51ilin9"))))
+        (base32 "1mq7332kgisd9yq0w0rv11vhwhgpkmpg7pfdlyn776dc13hcbqb1"))))
     (build-system gnu-build-system)
     (arguments `(#:configure-flags ',(malloc0-flags)))
     (inputs
