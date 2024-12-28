f4a41df4bb63e1b70132b8d976084797fce443e2
宋文武 <iyzsong@member.fsf.org>
2024-12-28 18:29:53 +0800

    gnu: xterm: Update to 396.
    * gnu/packages/xorg.scm (xterm): Update to 396.
    
    Change-Id: Ice1206e6a5a0ec96b84c73bdee3c8d4a44b89c5d

--- gnu/packages/xorg.scm
+++ gnu/packages/xorg.scm
@@ -5995,7 +5995,7 @@ to answer a question.  Xmessage can also exit after a specified time.")
 (define-public xterm
   (package
     (name "xterm")
-    (version "390")
+    (version "396")
     (source
      (origin
        (method url-fetch)
@@ -6005,7 +6005,7 @@ to answer a question.  Xmessage can also exit after a specified time.")
              (string-append "ftp://ftp.invisible-island.net/xterm/"
                             "xterm-" version ".tgz")))
        (sha256
-        (base32 "03wwdwnpj5dg1ah5sfp00ppzawjd81lnw47g4p20jjhpqly7q4bm"))
+        (base32 "19d6g33nryfg390jv4hdwic5bazj8vkm4qzlk6d23d7c1rnlpya3"))
        (patches
          (search-patches "xterm-370-explicit-xcursor.patch"))))
     (build-system gnu-build-system)
