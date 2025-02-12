dbb8bbb1955669cfc81a525c16f65ef1e839e067
Zheng Junjie <z572@z572.online>
2025-02-12 23:00:19 +0800

    squash! gnu: openssl-3.0: Update to 3.4.0.
    * gnu/packages/tls.scm (openssl-3.0)[arguments]: Remove apply-hurd-patch phase.
    * gnu/packages/patches/openssl-hurd64.patch: Remove it.
    * gnu/local.mk (dist_patch_DATA): Unregister it.
    
    Change-Id: If4400142886b9fe44e04ae3c083db2c7490f316c

--- gnu/packages/tls.scm
+++ gnu/packages/tls.scm
@@ -597,15 +597,6 @@ OpenSSL for TARGET."
                                            "/bin/perl"))))
             #$@(if (target-hurd?)
                    #~((delete 'patch-configure))
-                   #~())
-            #$@(if (target-hurd64?)
-                   #~((add-after 'unpack 'apply-hurd-patch
-                        (lambda _
-                          (let ((patch-file
-                                 #$(local-file
-                                    (search-patch "openssl-hurd64.patch"))))
-                            (invoke "patch" "--force" "-p1" "-i"
-                                    patch-file)))))
                    #~())))
        ((#:configure-flags flags #~'())
         (if (system-hurd?)              ;must not be used when
