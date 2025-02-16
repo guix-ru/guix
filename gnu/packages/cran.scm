cb74db59a13758dfb79e17e9c49a6a3d00516b82
Ricardo Wurmus <rekado@elephly.net>
2025-02-16 21:49:58 +0100

    gnu: r-distributional: Add test inputs.
    * gnu/packages/cran.scm (r-distributional)[properties]: Record
    updater-extra-native-inputs.
    [native-inputs]: Add r-evd and r-mvtnorm.
    
    Change-Id: I8e0daade94c406815580058fa9580a9bd917a9a7

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -40184,7 +40184,8 @@ vignette for more information and examples.")
         (base32
          "1b3i5vgljpjcr8v2xrz20cd900q36rmijizfgcx7id3raj5dwp1l"))))
     (properties
-     `((upstream-name . "distributional")))
+     '((upstream-name . "distributional")
+       (updater-extra-native-inputs . ("r-evd" "r-mvtnorm"))))
     (build-system r-build-system)
     (propagated-inputs
      (list r-generics
@@ -40193,7 +40194,7 @@ vignette for more information and examples.")
            r-pillar
            r-rlang
            r-vctrs))
-    (native-inputs (list r-testthat))
+    (native-inputs (list r-evd r-mvtnorm r-testthat))
     (home-page "https://pkg.mitchelloharawild.com/distributional/")
     (synopsis "Vectorized probability distributions")
     (description
