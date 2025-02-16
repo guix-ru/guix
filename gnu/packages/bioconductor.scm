a8dff20dac9c5e70fbbd8de32125052d7f5f505e
Ricardo Wurmus <rekado@elephly.net>
2025-02-16 21:03:17 +0100

    gnu: r-spatialexperiment: Disable tests.
    * gnu/packages/bioconductor.scm (r-spatialexperiment)[arguments]: Disable
    tests.
    
    Change-Id: Ia4434f4330b7f2d79590c5ecbcc99dc38e884d5b

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -24886,6 +24886,8 @@ data in the column sparse format.")
     (build-system r-build-system)
     (arguments
      (list
+      ;; 4 tests fail because they need Internet access.
+      #:tests? #false
       #:phases
       '(modify-phases %standard-phases
          (add-after 'unpack 'set-HOME
