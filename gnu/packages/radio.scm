bc99dbee7fd75c7d8578277788b5a784d5587a07
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-02-24 21:06:13 +0000

    gnu: packages: Remove redundant (gnu packages golang) from some modules.
    * gnu/packages/android.scm: Remove redundant (gnu packages golang) from
    used modules.
    * gnu/packages/bioinformatics.scm: Likewise.
    * gnu/packages/check.scm: Likewise.
    * gnu/packages/configuration-management.scm: Likewise.
    * gnu/packages/databases.scm: Likewise.
    * gnu/packages/emacs-xyz.scm: Likewise.
    * gnu/packages/file-systems.scm: Likewise.
    * gnu/packages/golang-check.scm: Likewise.
    * gnu/packages/hardware.scm: Likewise.
    * gnu/packages/hyperledger.scm: Likewise.
    * gnu/packages/irc.scm: Likewise.
    * gnu/packages/mail.scm: Likewise.
    * gnu/packages/messaging.scm: Likewise.
    * gnu/packages/networking.scm: Likewise.
    * gnu/packages/password-utils.scm: Likewise.
    * gnu/packages/radio.scm: Likewise.
    * gnu/packages/terminals.scm: Likewise.
    * gnu/packages/tor-browsers.scm: Likewise.
    * gnu/packages/video.scm: Likewise.
    * gnu/packages/vpn.scm: Likewise.
    
    Change-Id: I6bb92ac0f7c68644cc9b9cc16834aa54240d8fc9

--- gnu/packages/radio.scm
+++ gnu/packages/radio.scm
@@ -65,7 +65,6 @@
   #:use-module (gnu packages gl)
   #:use-module (gnu packages glib)
   #:use-module (gnu packages gnome)
-  #:use-module (gnu packages golang)
   #:use-module (gnu packages golang-xyz)
   #:use-module (gnu packages gps)
   #:use-module (gnu packages graphviz)
