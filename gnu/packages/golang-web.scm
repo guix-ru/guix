d39db76b0c44fa11a014182277701e33f0757dd2
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-02-25 23:54:19 +0000

    gnu: Add go-gitlab-com-gitlab-org-api-client-go.
    * gnu/packages/golang-web.scm (go-gitlab-com-gitlab-org-api-client-go): New variable.
    
    Change-Id: I1ddee93ce15b3882947b3a3d42c46463f004cc2f

--- gnu/packages/golang-web.scm
+++ gnu/packages/golang-web.scm
@@ -9201,6 +9201,54 @@ multiplexing.  The original intention of this library is to power the
 connection management for @url{https://github.com/xtaci/kcp-go,kcp-go}.")
     (license license:expat)))
 
+(define-public go-gitlab-com-gitlab-org-api-client-go
+  (package
+    (name "go-gitlab-com-gitlab-org-api-client-go")
+    (version "0.123.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://gitlab.com/gitlab-org/api/client-go.git")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0xrqyqvmaslfr9cj91519qi5f2sd3l2mvipxgjf13vd3v4fchacn"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:go go-1.22
+      #:import-path "gitlab.com/gitlab-org/api/client-go"
+      #:test-flags
+      #~(list "-skip"
+              (string-join
+               ;; Tests requir network access.
+               (list "TestGetMergeRequest"
+                     "TestRepositoryFilesService_CreateFile"
+                     "TestRepositoryFilesService_DeleteFile"
+                     "TestRepositoryFilesService_GetFile"
+                     "TestRepositoryFilesService_GetFileBlame"
+                     "TestRepositoryFilesService_GetFileMetaData"
+                     "TestRepositoryFilesService_GetRawFile"
+                     "TestRepositoryFilesService_UpdateFile"
+                     "TestRepositorySubmodulesService_UpdateSubmodule"
+                     "TestUpdateRepositoryEnvironmentsEscapesURL")
+               "|"))))
+    (native-inputs
+     (list go-github-com-stretchr-testify))
+    (propagated-inputs
+     (list go-github-com-google-go-querystring
+           go-github-com-hashicorp-go-cleanhttp
+           go-github-com-hashicorp-go-retryablehttp
+           go-golang-org-x-oauth2
+           go-golang-org-x-time))
+    (home-page "https://gitlab.com/gitlab-org/api/client-go")
+    (synopsis "GitLab client for Golang")
+    (description
+     "This package provides a GitLab API client enabling Go programs to
+interact with GitLab in a simple and uniform way.")
+    (license license:asl2.0)))
+
 (define-public go-gitlab-torproject-org-tpo-anti-censorship-geoip
   (package
     (name "go-gitlab-torproject-org-tpo-anti-censorship-geoip")
