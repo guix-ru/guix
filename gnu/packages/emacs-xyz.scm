7840f446c56d3480fcc9ba21318cea060dc45abc
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-03-02 15:02:09 +0100

    gnu: emacs-cov: Use implicit “ert-runner”.
    * gnu/packages/emacs-xyz.scm (emacs-cov)[arguments]: Drop #:test-command.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -12588,7 +12588,6 @@ and set HTML attributes.")
       (arguments
        (list
         #:emacs emacs-no-x              ;for libxml
-        #:test-command #~(list "ert-runner")
         #:phases
         #~(modify-phases %standard-phases
             (add-before 'check 'disable-failing-tests
