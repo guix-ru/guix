b3b17390a639b59bacc349f1680117fdfce5dd9b
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-03-02 15:11:08 +0100

    gnu: emacs-json-reformat: Use implicit “ert-runner”.
    * gnu/packages/emacs-xyz.scm (emacs-json-reformat)[arguments]:
    Drop #:test-command.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -22122,8 +22122,7 @@ running a customisable handler command (@code{ignore} by default).")
        (patches (search-patches "emacs-json-reformat-fix-tests.patch"))))
     (build-system emacs-build-system)
     (arguments
-     `(#:test-command '("ert-runner")
-       #:phases
+     `(#:phases
        (modify-phases %standard-phases
          (add-before 'check 'make-tests-writable
            (lambda _
