12989562a9669d93c032d5421edf9bec0dc6789a
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-03-02 15:12:50 +0100

    gnu: emacs-helm-company: Use implicit “ert-runner”.
    * gnu/packages/emacs-xyz.scm (emacs-helm-company): Drop arguments.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -29475,8 +29475,6 @@ tracking.")
      (list emacs-helm emacs-company))
     (native-inputs
      (list emacs-ert-runner))
-    (arguments
-     (list #:test-command #~(list "ert-runner")))
     (home-page "https://github.com/Sodel-the-Vociferous/helm-company")
     (synopsis "Helm interface for Company mode")
     (description
