6445073acf8139f682f9dcbe39c5564b2b58e718
Greg Hogan <code@greghogan.com>
2025-03-08 20:04:11 +0000

    gnu: fbgemm: Use default cmake.
    * gnu/packages/machine-learning.scm (fbgemm)
    [arguments]<#:cmake>: Delete.
    
    Change-Id: I536d07975ebdad012b82c99489585e1fcd967a2b

--- gnu/packages/machine-learning.scm
+++ gnu/packages/machine-learning.scm
@@ -4583,7 +4583,6 @@ TensorFlow.js, PyTorch, and MediaPipe.")
     (build-system cmake-build-system)
     (arguments
      (list
-      #:cmake cmake-3.30
       #:configure-flags
       ''("-DFBGEMM_LIBRARY_TYPE=shared")
       ;; Tests require AVX2 or AVX-512 instructions
