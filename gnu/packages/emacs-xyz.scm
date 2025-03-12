2ef926a578b9f0b786628197498f1c4a1eea596c
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-03-12 08:51:11 +0100

    gnu: emacs-clue: Skip tests.
    * gnu/packages/emacs-xyz.scm (emacs-clue)[arguments]: Add #:tests? #f.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -19604,6 +19604,7 @@ stuff (words, region, lines) around in Emacs.")
                 (sha256
                  (base32 "08xpdpac82v5vwqqqgbh5imakl4pys6bpfacfk05pk88lw925ql8"))))
       (build-system emacs-build-system)
+      (arguments (list #:tests? #f))    ; no tests
       (home-page "https://github.com/AmaiKinono/clue/")
       (synopsis "Connecting clues while reading code")
       (description
