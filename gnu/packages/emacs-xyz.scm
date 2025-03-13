bbe15f8b3d5e431ab8a8fc0627a0230980950cc1
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-03-13 20:42:57 +0100

    gnu: emacs-epkg: Skip tests.
    * gnu/packages/emacs-xyz.scm (emacs-epkg)[arguments]: Add #:tests? #f.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -25190,6 +25190,7 @@ match your personal coding style.")
     (build-system emacs-build-system)
     (arguments
      `(#:lisp-directory "lisp"
+       #:tests? #f                      ; no tests
        #:phases
        (modify-phases %standard-phases
          (add-before 'install 'make-info
