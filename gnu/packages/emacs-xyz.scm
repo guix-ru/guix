417ee14b6b95c1e0539f312513f4b7cd89e9845a
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-03-16 18:51:15 +0100

    gnu: emacs-org-roam: Fix tests.
    * gnu/packages/emacs-xyz.scm (emacs-org-roam)[arguments]: Add #:test-command.
    [native-inputs]: Add emacs-ert-runner.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -40347,6 +40347,7 @@ go directly to where they belong.")
       (build-system emacs-build-system)
       (arguments
        (list
+        #:test-command #~(list "ert-runner" "tests")
         #:phases
         #~(modify-phases %standard-phases
             ;; Move the extensions source files to the top level, which
@@ -40376,7 +40377,7 @@ go directly to where they belong.")
       (inputs
        (list graphviz))
       (native-inputs
-       (list texinfo))
+       (list emacs-ert-runner texinfo))
       (propagated-inputs
        (list emacs-dash
              emacs-emacsql
