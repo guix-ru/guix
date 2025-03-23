ee609e51826d14fdf88d01432f1e474278b02238
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-03-23 10:52:15 +0100

    gnu: emacs-telega: Fix tests.
    * gnu/packages/emacs-xyz.scm (emacs-telega)[arguments]: Add #:test-command.
    <#:phases>: Move ‘check’ after ‘install’.
    Add ‘set-home’ and ‘skip-ensure-dependencies’.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -38280,6 +38280,7 @@ service, and connect it with Emacs via inter-process communication.")
                     emacs-minimal
                     ;; Require wide-int support for 32-bit platform.
                     emacs-wide-int)
+        #:test-command #~(list "make" "test_el")
         #:include #~(cons "^etc\\/" %default-include)
         #:phases
         #~(modify-phases %standard-phases
@@ -38312,7 +38313,17 @@ service, and connect it with Emacs via inter-process communication.")
                 (substitute* "telega-core.el"
                   (("@TELEGA_SHARE@")
                    (string-append (elpa-directory (assoc-ref outputs "out"))
-                                  "/etc"))))))))
+                                  "/etc")))))
+            (delete 'check)
+            (add-after 'install 'check (assoc-ref %standard-phases 'check))
+            (add-before 'check 'set-home
+              (lambda _
+                (setenv "HOME" (getenv "TMPDIR"))))
+            (add-before 'check 'skip-ensure-dependencies
+              (lambda _
+                (substitute* "etc/telega-make.el"
+                  (("\\(telega-ensure-dependencies\\)" all)
+                   (string-append ";; " all))))))))
       (inputs
        (list emacs-telega-server ffmpeg tgs2png))
       (native-inputs '())
