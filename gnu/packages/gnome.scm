36542354e07cf50a010645725a7f4f780da3a1e1
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2025-05-19 14:01:22 +0900

    gnu: gdm: Update to 48.0.
    * gnu/packages/gnome.scm (gdm): Update to 48.0.
    * gnu/packages/patches/gdm-default-session.patch: Adjust accordingly.
    * gnu/packages/patches/gdm-remove-hardcoded-xwayland-path.patch: Likewise.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -8877,7 +8877,7 @@ library.")
 (define-public gdm
   (package
     (name "gdm")
-    (version "46.2")
+    (version "48.0")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnome/sources/" name "/"
@@ -8885,7 +8885,7 @@ library.")
                                   name "-" version ".tar.xz"))
               (sha256
                (base32
-                "12r6ppsdq9f0rlhfwldwhilshb1blp6m0944rm872lqn5914bqsf"))
+                "1pqaqg8ngs8y2nwysxm03cdllix3blpgkd7c6xg7pv4ky2pnvh0v"))
               (patches
                (search-patches
                 "gdm-default-session.patch"
