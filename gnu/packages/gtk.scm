c7576fdb88f3af54dd6fdaeacbfafad2da317f29
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2025-05-19 14:01:24 +0900

    gnu: gtkmm: Update to 4.18.0.
    * gnu/packages/gtk.scm (gtkmm): Update to 4.18.0.
    
    Change-Id: I50d661bc5865f14c3b43b3f95c055a9304eb1373

--- gnu/packages/gtk.scm
+++ gnu/packages/gtk.scm
@@ -1936,7 +1936,7 @@ text rendering library.")
 (define-public gtkmm
   (package
     (name "gtkmm")
-    (version "4.14.0")
+    (version "4.18.0")
     (source
      (origin
        (method url-fetch)
@@ -1945,7 +1945,7 @@ text rendering library.")
                        (version-major+minor version)  "/"
                        name "-" version ".tar.xz"))
        (sha256
-        (base32 "1npcf6i07riw20rg5x6rnqi0jlh7cwdvsvjqd7fa6k3l9d2a0l4k"))))
+        (base32 "1v1kkrhhakkv952gb8n24b0ppqf8pdgqng5hb3lxii4z8wairqrf"))))
     (build-system meson-build-system)
     (outputs '("out" "doc"))
     (arguments
