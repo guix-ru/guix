eccb715e757cc8b0073f30ec4de1a5b9e2b71dd5
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2025-06-02 09:08:43 +0900

    gnu: Remove #:go arguments using go-1.22, go-1.23 or go-1.24.
    We are now building with go-1.24 by default.
    
    Automated via:
    
      git grep -rl '#:go go-1.2' | xargs sed -i '/.*#:go go-1.2[234].*/d'
    
    Change-Id: Ib821d96753619c7748a67ff58b95d8fa358839b7
    Modified-by: Sharlatan Hellseher <sharlatanus@gmail.com>
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/web.scm
+++ gnu/packages/web.scm
@@ -402,7 +402,6 @@ one.")
     (build-system go-build-system)
     (arguments
      (list
-      #:go go-1.22
       #:install-source? #f
       #:import-path "miniflux.app/v2"
       #:build-flags
@@ -5811,7 +5810,6 @@ processor.")
     (build-system go-build-system)
     (arguments
      (list
-      #:go go-1.22
       #:embed-files #~(list ".*.xml")
       #:install-source? #f
       #:import-path "github.com/noahgorstein/jqp"))
