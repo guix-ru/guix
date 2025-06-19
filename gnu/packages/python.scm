362ee1b9d59c650c69015812b24464c0a808c1da
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-06-19 20:17:39 +0200

    [WORKAROUND] gnu: python: Fix build with unprivileged daemon.
    * gnu/packages/python.scm (python-3.10, python-3.11)[#:make-flags]: Also
    exclude “test_os” and “test_subprocess”.

--- gnu/packages/python.scm
+++ gnu/packages/python.scm
@@ -500,6 +500,8 @@ data types.")
                  (format #f "TESTOPTS=-j~d" (parallel-job-count))
                  ;; test_mmap fails on low-memory systems
                  " --exclude test_mmap test_socket"
+                 " test_os"              ; broken with unprivileged daemon
+                 " test_subprocess"      ; broken with unprivileged daemon
                  #$@(if (system-hurd?)
                         #~(" test_posix" ;multiple errors
                            " test_time"
@@ -766,6 +768,8 @@ def contents() -> str:
                             " test_threading"
                             " test_asyncio"
                             " test_shutdown"
+                            " test_os"              ; broken with unprivileged daemon
+                            " test_subprocess"      ; broken with unprivileged daemon
                             ,@(if (system-hurd?)
                                   '(" test_posix" ;multiple errors
                                     " test_time"
