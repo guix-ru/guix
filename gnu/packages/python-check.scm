65d4854c533db7b7c32b623bea2d5757ce64ed04
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-07-12 21:01:02 +0100

    gnu: python-mypy: Update to 1.16.1.
    * gnu/packages/python-check.scm (python-mypy): Update to 1.16.1.
      [arguments] <test-flags>: Respect parallel option.
      [native-inputs]: Remove python-attrs, python-pytest-forked, and
      python-virtualenv; add  python-types-setuptools.
      [propagated-inputs]: Add python-pathspec.
    
    Change-Id: Idaecb763785b541f2591cd4530cb71a91e973f44

--- gnu/packages/python-check.scm
+++ gnu/packages/python-check.scm
@@ -1247,24 +1247,25 @@ library of the same name.  It eases monkey patching, for example to stub out
 side effects when unit testing.")
     (license license:expat)))
 
-
 (define-public python-mypy
   (package
     (name "python-mypy")
-    (version "1.13.0")
+    (version "1.16.1")
     (source
      (origin
        (method url-fetch)
        (uri (pypi-uri "mypy" version))
        (sha256
         (base32
-         "0pl3plw815824z5gsncnjg3yn2v5wz0gqp20wdrncgmzdwdsd482"))))
+         "1avv8cj0qfhpw4s36bjhg994rml35fs4ndz78xg1r14l4050ml3b"))))
     (build-system pyproject-build-system)
     (arguments
      (list
-      ;; It tries to download hatchling and install aditional test
-      ;; dependencies.
-      #:test-flags #~(list "--ignore=mypy/test/testpep561.py")
+      #:test-flags
+      #~(list "--numprocesses" (number->string (parallel-job-count))
+              ;; It tries to download hatchling and install aditional test
+              ;; dependencies.
+              "--ignore=mypy/test/testpep561.py")
       #:phases
       #~(modify-phases %standard-phases
           (add-before 'check 'set-home
@@ -1275,17 +1276,16 @@ side effects when unit testing.")
               (setenv "HOME" "/tmp"))))))
     (native-inputs
      (list nss-certs-for-test
-           python-attrs
            python-lxml
            python-psutil
            python-pytest
-           python-pytest-forked
            python-pytest-xdist
            python-setuptools
-           python-virtualenv
+           python-types-setuptools
            python-wheel))
     (propagated-inputs
      (list python-mypy-extensions
+           python-pathspec
            python-tomli
            python-typing-extensions))
     (home-page "https://www.mypy-lang.org/")
