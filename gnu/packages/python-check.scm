af1bdb2a94ed8b1668861a09ab0bb307b68ecb9f
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-07-15 13:56:56 +0100

    gnu: python-ddt: Fix build.
    * gnu/packages/python-check.scm (python-ddt) [native-inputs]: Add
      python-six.
    
    Change-Id: I0770740a27b8c37c2c5091366c02f36ebba3bacf

--- gnu/packages/python-check.scm
+++ gnu/packages/python-check.scm
@@ -620,6 +620,7 @@ counterexamples for you.")
      (list python-aiounittest
            python-pytest
            python-setuptools
+           python-six
            python-wheel))
     (propagated-inputs
      (list python-pyyaml))
