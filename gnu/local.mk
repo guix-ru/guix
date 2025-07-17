1c7d5a45329c09f7fa5ee27157379aed827ae3c2
Maxim Cournoyer <maxim@guixotic.coop>
2025-07-17 16:58:55 +0900

    gnu: aspell: Update to 0.60.8.1.
    * gnu/packages/aspell.scm (aspell): Update to 0.60.8.1.
    [patches]: Remove aspell-CVE-2019-25051.patch.
    [phases]: Delete trailing #t.
    * gnu/packages/patches/aspell-CVE-2019-25051.patch: Delete file.
    * gnu/local.mk (dist_patch_DATA): Unregister it.
    
    Change-Id: I58f8e51d084415f88ce7f0d5b31b23bbf4084018

--- gnu/local.mk
+++ gnu/local.mk
@@ -1003,7 +1003,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/aria2-unbundle-wslay.patch		\
   %D%/packages/patches/ark-skip-xar-test.patch			\
   %D%/packages/patches/asli-use-system-libs.patch		\
-  %D%/packages/patches/aspell-CVE-2019-25051.patch		\
   %D%/packages/patches/aspell-default-dict-dir.patch		\
   %D%/packages/patches/atf-execute-with-shell.patch		\
   %D%/packages/patches/ath9k-htc-firmware-binutils.patch	\
