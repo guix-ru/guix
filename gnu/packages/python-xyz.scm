8a5e93299eee6134c5dfeb4658df083cec1bcb2f
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-07-17 23:47:02 +0100

    gnu: python-dbus-python: Reduce closure size.
    Tests never run before and silently were skipped because test-backend is
    not known for pyrpoject-build-system which is tappy, this change
    visually disables tests and remove some unused inputs from closure.
    
    * gnu/packages/python-xyz.scm (python-dbus-python):
      [native-inputs]: Remove python-sphinx, python-sphinx-rtd-theme, and
      python-wheel.
    
    Change-Id: I042ceace8f42ae8514751ffe0fee664db05ce2cc

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -31739,6 +31739,7 @@ Its algorithms are based on the kakasi library, which is written in C.")
     (build-system pyproject-build-system)
     (arguments
      (list
+      #:tests? #f ;XXX: Tests require setup
       #:phases
       #~(modify-phases %standard-phases
           (add-after 'unpack 'patch-requirements
@@ -31749,15 +31750,12 @@ Its algorithms are based on the kakasi library, which is written in C.")
                 (("(ninja|patchelf)") "")))))))
     (native-inputs
      (list pkg-config
-           python-meson-python
            meson
            ninja
            patchelf
+           python-meson-python
            python-setuptools
-           python-sphinx
-           python-sphinx-rtd-theme
-           python-tappy
-           python-wheel))
+           python-tappy))
     (inputs
      (list dbus glib))
     (propagated-inputs
