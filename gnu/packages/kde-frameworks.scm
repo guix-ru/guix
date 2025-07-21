05768af0892146a5016434352a9b4d9ed3c287ba
Zheng Junjie <z572@z572.online>
2025-07-21 16:12:30 +0800

    gnu: syndication: Update to 6.16.0.
    * gnu/packages/kde-frameworks.scm (syndication): Update to 6.16.0.
    
    Change-Id: I8a3781b98ee9faa1f9e722ebc9b2adc37cee91e9

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -3082,7 +3082,7 @@ gallons).")
 (define-public syndication
   (package
     (name "syndication")
-    (version "6.13.0")
+    (version "6.16.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -3091,7 +3091,7 @@ gallons).")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "1dwgl0nqc8lpkcs7hnd642q2ixbvjw4yx9wmz59rfr852xc7mq0z"))))
+                "1pmndjdcsz9siphhrnrn5xslrja4zv96qkh2fj0q2489f9q547wh"))))
     (build-system cmake-build-system)
     (native-inputs
      (list extra-cmake-modules))
