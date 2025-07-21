720e07302c012aaae178d8c1f9a612c87fee90ce
Zheng Junjie <z572@z572.online>
2025-07-21 16:23:55 +0800

    gnu: kglobalaccel: Update to 6.16.0.
    * gnu/packages/kde-frameworks.scm (kglobalaccel): Update to 6.16.0.
    
    Change-Id: I61532d015952b28099cfc995baf6572d85f4cac5

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -3681,7 +3681,7 @@ emoticons coming from different providers.")
 (define-public kglobalaccel
   (package
     (name "kglobalaccel")
-    (version "6.13.0")
+    (version "6.16.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -3690,7 +3690,7 @@ emoticons coming from different providers.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "1c4nzr778qlgw67zmd7nzbi7ylyg8al69fzxkibg8mzjiimg2m4r"))))
+                "18aapp3xlrgllwxmqabyz18wj4kyqg092kplp765v2vrh8d4cqv6"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules pkg-config qttools))
