8c3d674b65b75c656933524730133ea02774b2da
Murilo <murilo@disroot.org>
2025-07-24 09:01:38 -0300

    gnu: hyprland: Build with libinput-minimal.
    * gnu/packages/wm.scm (hyprland)[inputs]: Change libinput-minimal-next
    to libinput-minimal.
    
    Signed-off-by: John Kehayias <john.kehayias@protonmail.com>

--- gnu/packages/wm.scm
+++ gnu/packages/wm.scm
@@ -428,7 +428,7 @@ loginctl commands (lock/unlock/before-sleep) and inhibit.")
            hyprland-qtutils
            hyprlang
            hyprutils
-           libinput-minimal-next
+           libinput-minimal
            libxcursor
            libxkbcommon
            mesa
