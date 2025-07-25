1df07db9576f6e4ebe15563c98f5e1f19d8ab112
Greg Hogan <code@greghogan.com>
2025-07-25 12:44:37 +0000

    gnu: glib-networking: Skip flaky tests.
    * gnu/packages/gnome.scm (glib-networking)[arguments]<#:phases>: Add
    'mark-tests-for-exclusion phase.
    <#:test-options>: Exclude flaky tests.
    
    Change-Id: I535c57eb90e1629e9c1d4cb468da6c183e51bcf4

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -5049,6 +5049,23 @@ indicators etc).")
                (base32
                 "17zhkf2pjwrghdgk5nhfvzqakb2xwk2jj19316xjr0s9n3djv3z4"))))
     (build-system meson-build-system)
+    (arguments
+     (list
+      ;; Exclude flaky tests (https://codeberg.org/guix/guix/issues/1377).
+      ;; Meson cannot exclude individual tests so the test suite is added
+      ;; in the phase below.
+      #:test-options #~(list "--no-suite" "connection")
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'mark-tests-for-exclusion
+            (lambda _
+              ;; The test names are programmatically generated in the meson
+              ;; build file. The two failing tests are "connection-gnutls" and
+              ;; "connection-gnutls-tls1.2" and share program[0] == "common",
+              ;; so use that as the suite name.
+              (substitute* "tls/tests/meson.build"
+                (("test\\(([^)]*)\\)" _ args)
+                 (string-append "test(" args ", suite: program[0])"))))))))
     (native-inputs
      (list `(,glib "bin") ; for gio-querymodules
            pkg-config gettext-minimal))
