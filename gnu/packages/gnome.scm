20b2f01e8c737544fd46fc449f28ae59c3e74abc
Maxim Cournoyer <maxim@guixotic.coop>
2025-07-26 22:57:52 +0900

    gnu: yelp-xsl: Update to 42.4.
    * gnu/packages/gnome.scm (yelp-xsl): Update to 42.4.
    [build-system]: Switch to meson-build-system.
    
    Change-Id: I97ae90008d3e9d14261b14af9f04538fe5e9ed40

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -7338,7 +7338,7 @@ environments.")
 (define-public yelp-xsl
   (package
     (name "yelp-xsl")
-    (version "42.1")
+    (version "42.4")
     (source
      (origin
        (method url-fetch)
@@ -7347,10 +7347,8 @@ environments.")
                        (version-major version) "/"
                        name "-" version ".tar.xz"))
        (sha256
-        (base32 "1kav039g62q35h508shdbrcjcfkdsc7k6wcr2g780c35n58f32r3"))))
-    (build-system gnu-build-system)
-    (arguments
-     '(#:configure-flags '("--enable-doc")))
+        (base32 "1lfpmdc4y46a528q5filn6xs8add921avrnwl2vpysp6n9zb1szx"))))
+    (build-system meson-build-system)
     (native-inputs
      (list mallard-ducktype
            gettext-minimal
