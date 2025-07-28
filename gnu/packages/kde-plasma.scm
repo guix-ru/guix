c726195e6773f0e6c8763a746bc8dd750cdb89a4
Zheng Junjie <z572@z572.online>
2025-07-28 14:26:00 +0800

    gnu: libksysguard: Update to 6.4.3.
    * gnu/packages/kde-plasma.scm (libksysguard): Update to 6.4.3.
    
    Change-Id: I8acb05e63ef864b374119f69fa5f39d6d72e711c

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -1264,14 +1264,14 @@ basic needs and easy to configure for those who want special setups.")
 (define-public libksysguard
   (package
     (name "libksysguard")
-    (version "6.3.4")
+    (version "6.4.3")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/plasma/" version
                            "/libksysguard-" version ".tar.xz"))
        (sha256
-        (base32 "1p0mrgbzkqakrfwdrfjn70prn1wkxcpd9qpisvwdajhp3a5yxh84"))))
+        (base32 "1qijnyz6f4kk2y3zl4c369sbbrzrdbndi6g3dr30w2yjbwqm78sp"))))
     (native-inputs
      (list bash-minimal extra-cmake-modules pkg-config qttools))
     (inputs
