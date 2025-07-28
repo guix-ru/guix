3e6998b384613e80bef5f45b6d0981b4fd6441a0
Maxim Cournoyer <maxim@guixotic.coop>
2025-07-28 14:25:32 +0900

    gnu: libnotify: Disable tests.
    Few seem to run libnotify's test suite, including upstream's CI.
    
    * gnu/packages/gnome.scm (libnotify) [#:tests?]: Set to #f.
    
    Change-Id: I99b3d4bda48809614f816b08e875f900b71f88d4

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -3124,6 +3124,9 @@ configuring CUPS.")
     (build-system meson-build-system)
     (arguments
      (list
+      ;; The recently added tests would require a notification daemon, which
+      ;; introduce a circular dependency with libnotify.
+      #:tests? #f
       #:phases
       #~(modify-phases %standard-phases
           (add-after 'install 'move-doc
