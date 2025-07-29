8e5a09abec8d95f36a39d22370234269aedb32fa
Efraim Flashner <efraim@flashner.co.il>
2025-07-29 11:17:04 +0300

    fixup gmp-boot
    Change-Id: I0d38c62e98607fa5cdfa08b3e2c2fad67be927a4

--- gnu/packages/commencement.scm
+++ gnu/packages/commencement.scm
@@ -1666,14 +1666,13 @@ ac_cv_c_float_format='IEEE (little-endian)'
        #:guile %bootstrap-guile
        #:tests? #f
        #:implicit-inputs? #f
-       #:parallel-build? (target-x86?)
+       #:parallel-build? #f
        #:configure-flags
        #~(list #$@(if (target-x86?)
                       #~()
                       #~("CC=tcc"))
-               ;; These break building on x86_64-linux.
-               ;(string-append "--build=" #$(commencement-build-target))
-               ;(string-append "--host=" #$(commencement-build-target))
+               (string-append "--build=" #$(commencement-build-target))
+               (string-append "--host=" #$(commencement-build-target))
                "--enable-static"
                "--disable-shared"
                "--disable-assembly")
