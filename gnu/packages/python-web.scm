723e20a227e403bf11528070220088f244959fa9
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-07-31 16:32:20 +0100

    gnu: python-tornado: Switch to pypproject-build-system.
    * gnu/packages/python-web.scm (python-tornado)
      [build-system]: Use pypproject.
      [native-inputs]: Add python-setuptools.
    
    Change-Id: I39e3a906f4ac66b878a4aae157cef5fbd1135074

--- gnu/packages/python-web.scm
+++ gnu/packages/python-web.scm
@@ -4099,6 +4099,7 @@ data.")
 
 (define-public python-tornado
   (package
+    ;; TODO: Try to refresh and check all dependents.
     (name "python-tornado")
     (version "5.1.1")
     (source
@@ -4108,7 +4109,7 @@ data.")
        (sha256
         (base32
          "02clqk2116jbnq8lnaqmdw3p52nqrd9ib59r4xz2ll43fpcmhlaf"))))
-    (build-system python-build-system)
+    (build-system pyproject-build-system)
     (arguments
      (list
       #:phases
@@ -4138,7 +4139,8 @@ data.")
                (invoke "python" "-m" "tornado.test.runtests"
                        "--verbose=yes")))))))
     (native-inputs
-     (list python-certifi))
+     (list python-certifi
+           python-setuptools))
     (home-page "https://www.tornadoweb.org/")
     (synopsis "Python web framework and asynchronous networking library")
     (description
