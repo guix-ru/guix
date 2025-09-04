0a009be290b7f0144d6b1f711091a96d0230f22e
Maxim Cournoyer <maxim@guixotic.coop>
2025-09-04 15:04:30 +0900

    WIP: gnu: qtwebengine: Update to 6.9.2.
    WIP while https://bugreports.qt.io/browse/QTBUG-139959 is unresolved.
    
    * gnu/packages/qt.scm (qtwebengine): Update to 6.9.2.
    [snippet]: Adjust for removed or moved and added bundled library dependencies.
    [native-inputs]: Replace clang-18 with clang-20.
    [inputs]: replace icu4c-75 with icu4c. Delete fp16, which is bundled.
    
    Change-Id: I31b2b3ae2a9240aaa72290cfc0467a04af18c91c

--- gnu/local.mk
+++ gnu/local.mk
@@ -2211,7 +2211,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/qtbase-qmake-use-libname.patch		\
   %D%/packages/patches/qtbase-5-use-TZDIR.patch			\
   %D%/packages/patches/qtscript-disable-tests.patch		\
-  %D%/packages/patches/qtwebengine-fix-system-libvpx.patch	\
   %D%/packages/patches/quagga-reproducible-build.patch          \
   %D%/packages/patches/quickswitch-fix-dmenu-check.patch	\
   %D%/packages/patches/quilt-grep-compat.patch 			\
