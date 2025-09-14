76304688d2f59af6e53b0382cc0c0aaf7c4e9daa
Nicolas Graves <ngraves@ngraves.fr>
2025-09-14 23:17:51 +0200

    gnu: librsvg: Extend with (cargo-guile-json).
    * gnu/packages/gnome.scm (librsvg)[arguments]
    <#:phases>: Extend with (cargo-guile-json).
    
    Change-Id: Idd8c2c0681ffdc2a5cf3a73f0b1b606db38994c5
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -3660,6 +3660,7 @@ for dealing with different structured file formats.")
         (guix build gnu-build-system)
         (guix build utils))
       #:phases
+      (with-extensions (list (cargo-guile-json))
       #~(modify-phases %standard-phases
           (add-after 'unpack 'patch-gdk-pixbuf-thumbnailer
             (lambda* (#:key inputs #:allow-other-keys)
@@ -3711,7 +3712,7 @@ for dealing with different structured file formats.")
                '(unpack-rust-crates
                  configure
                  check-for-pregenerated-files
-                 patch-cargo-checksums)))))))
+                 patch-cargo-checksums))))))))
     (native-inputs
      (append
       (list gdk-pixbuf
