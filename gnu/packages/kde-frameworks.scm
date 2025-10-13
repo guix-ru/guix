c84942f233a81d384b3566a10d5f99e9a3d14e79
Sughosha <sughosha@disroot.org>
2025-10-13 20:46:49 +0530

    gnu: knotifyconfig: Update to 6.19.0.
    * gnu/packages/kde-frameworks.scm (knotifyconfig): Update to 6.19.0.
    [inputs]: Add libcanberra.
    
    Change-Id: I91bf7c2cd745bf75fd50cad9541df1f09c5288e7

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -4176,7 +4176,7 @@ specification.")
 (define-public knotifyconfig
   (package
     (name "knotifyconfig")
-    (version "6.18.0")
+    (version "6.19.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -4185,7 +4185,7 @@ specification.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "1a5d34x0bzfa7gy1ch059sh1lks6pyywdb8b6waf0fw3w6s7z0cs"))))
+                "07y3qnsrxc12d32f9w7is17ka31ynf2z0mpl3g6kj0b3d2x366fd"))))
     (build-system cmake-build-system)
     (arguments (list #:tests? #f))
     (native-inputs
@@ -4207,6 +4207,7 @@ specification.")
            kservice
            kwidgetsaddons
            kxmlgui
+           libcanberra
            phonon
            qtbase
            qtmultimedia
