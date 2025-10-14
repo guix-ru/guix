ece72273c22ab09cd7dbc2cced33f74d4958c1be
Ada Stevenson <adanskana@gmail.com>
2025-10-14 17:00:14 +0800

    gnu: upower: Replace python-dbus for python-dbus-python.
    python-dbus and python-dbus-python appear to be the same package, with
    different versions.  A follow-up commit should deprecate the former in favour
    of the latter.
    
    * gnu/packages/gnome.scm (upower)[native-inputs]: Replace python-dbus with
    python-dbus-python.
    
    Change-Id: I5efb4eeba4b5d8de110cc4d72bcc61d737a93f61
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -5851,7 +5851,7 @@ faster results and to avoid unnecessary server load.")
            pkg-config
            python
            ;; For tests.
-           python-dbus
+           python-dbus-python
            python-dbusmock-minimal
            python-packaging
            python-pygobject
