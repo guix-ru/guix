105012a2f0214f374513f202a5de7b18ac6dd41b
Maxim Cournoyer <maxim@guixotic.coop>
2025-10-22 12:39:00 +0900

    gnu: flex: Move bison-for-tests input to native inputs.
    If it's truly for tests, it ought to be a native inputs.
    
    * gnu/packages/compiler-tools.scm (flex) [inputs]: Delete field, moving bison-for-tests...
    [native-inputs]: ... here.
    
    Change-Id: I7fa30de00b1a6386d495403a619f412daa9d1ab5
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/compiler-tools.scm
+++ gnu/packages/compiler-tools.scm
@@ -139,19 +139,19 @@ prefixes of other terminals.")
                    "ac_cv_func_realloc_0_nonnull=yes"))
          '()))
     (native-inputs
-     (list help2man
-           ;; m4 is not present in PATH when cross-building
-           m4))
-    (inputs
-     (list
-      (package
-        (inherit bison)
-        (arguments
-         ;; Disable tests, since they require flex.
-         (substitute-keyword-arguments arguments
-           ((#:tests? _ #f) #f)))
-        (inputs (modify-inputs inputs
-                  (delete "flex"))))))
+     (let ((bison-for-tests
+            (package
+              (inherit bison)
+              (arguments
+               ;; Disable tests, since they require flex.
+               (substitute-keyword-arguments arguments
+                 ((#:tests? _ #f) #f)))
+              (inputs (modify-inputs inputs
+                        (delete "flex"))))))
+       (list bison-for-tests
+             help2man
+             ;; m4 is not present in PATH when cross-building
+             m4)))
     (propagated-inputs (list m4))
     (home-page "https://github.com/westes/flex")
     (synopsis "Fast lexical analyser generator")
