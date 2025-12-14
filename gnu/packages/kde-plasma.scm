878a8910a53296878d8c3591538dd842d9429567
Sughosha <sughosha@disroot.org>
2025-12-14 14:41:05 +0530

    gnu: kscreenlocker: Update to 6.5.4.
    * gnu/packages/kde-plasma.scm (kscreenlocker): Update to 6.5.4.
    
    Change-Id: Ia8f6c146f23794babcb223717ed03b5151ec816f

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -1265,14 +1265,14 @@ the running system.")
 (define-public kscreenlocker
   (package
     (name "kscreenlocker")
-    (version "6.5.2")
+    (version "6.5.4")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/plasma/" version
                                   "/kscreenlocker-" version ".tar.xz"))
               (sha256
                (base32
-                "1dh41lfqik7pzqk7px6jmbv68pm9ny1mmp8d6x20qjlni158j2fh"))))
+                "169qcb460jsk7jh29y6zafcqj0ib7kfdwcd6s3sm8sj8dfl22027"))))
     (build-system qt-build-system)
     (arguments
      (list #:tests? #f ;TODO: make tests pass
