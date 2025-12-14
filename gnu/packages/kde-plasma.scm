48b0962c89787ea8f8b1ee9bbda7e2729d043152
Sughosha <sughosha@disroot.org>
2025-12-14 14:41:01 +0530

    gnu: kde-gtk-config: Update to 6.5.4.
    * gnu/packages/kde-plasma.scm (kde-gtk-config): Update to 6.5.4.
    
    Change-Id: I8d3ead64716055afa8d69d15a91bb8a614f09aff

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -598,14 +598,14 @@ RDP server.")
 (define-public kde-gtk-config
   (package
     (name "kde-gtk-config")
-    (version "6.5.2")
+    (version "6.5.4")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/plasma/" version
                                   "/kde-gtk-config-" version ".tar.xz"))
               (sha256
                (base32
-                "0acd9vg2m9kjs4i4ys1vqwrgp8zp2caa52rwcrvfc35y04scsdyc"))))
+                "1a14xnniyakaz828m70j4mdh2i3sg9c8a6vb244xn6k98xn4jirp"))))
     (build-system qt-build-system)
     (arguments
      (list
