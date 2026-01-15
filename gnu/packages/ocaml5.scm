f8485897aaf4bb80537f5adf5b8a207d7c81065c
Jason Conroy <jconroy@tscripta.net>
2026-01-15 13:43:29 -0500

    gnu: ocaml5: Add ocaml-time-now.
    * gnu/packages/ocaml5.scm (ocaml-time-now): New variable.
    
    Change-Id: I938ec70ea565510173f2778924eb136cfd59287b
    Signed-off-by: Julien Lepiller <julien@lepiller.eu>

--- gnu/packages/ocaml5.scm
+++ gnu/packages/ocaml5.scm
@@ -1966,6 +1966,24 @@ Note that Base doesn't need ppx to build, it is only used as a
 verification tool.")
     (license license:expat)))
 
+(define-public ocaml-time-now
+  (package
+    (name "ocaml5-time-now")
+    (version "0.17.0")
+    (source
+     (janestreet-git-origin "time_now" version
+      "1abn5fqqixlj1jbqb6vwysn48m0fv9cp7jyw5nfkkyxivw9xccvd"))
+    (build-system dune-build-system)
+    (propagated-inputs (list ocaml-base ocaml-jane-street-headers
+                             ocaml-jst-config ocaml-ppx-base ocaml-ppx-optcomp))
+    (properties `((upstream-name . "time_now")))
+    (home-page "https://github.com/janestreet/time_now")
+    (synopsis "Reports the current time")
+    (description
+     "Provides a single function to report the current time in nanoseconds
+since the start of the Unix epoch.")
+    (license license:expat)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
