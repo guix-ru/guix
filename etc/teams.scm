2a7acb7ed5b7fc037ce5c5f61493a1d62ded2754
Cayetano Santos <csantosb@inventati.org>
2026-01-15 19:17:45 +0100

    etc: Add csantosb to reproduciblebuilds team.
    * etc/teams.scm: Make csantosb a member of the reproduciblebuilds team.
    
    Change-Id: I5228f754a67fbfcc6dd0ea72ec441c86963e47b3

--- etc/teams.scm
+++ etc/teams.scm
@@ -1308,7 +1308,7 @@ the \"texlive\" importer."
 (define-member (person "Cayetano Santos"
                        "csantosb@inventati.org"
                        "csantosb")
-  ai emacs electronics hpc crypto)
+  ai emacs electronics hpc crypto reproduciblebuilds)
 
 (define-member (person "Greg Hogan"
                        "code@greghogan.com"
