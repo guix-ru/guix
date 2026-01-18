bb97082cf63ed97147c48e8899b649b3d1df9283
Nicolas Graves <ngraves@ngraves.fr>
2026-01-18 21:37:38 +0100

    gnu: python-cattrs: Drop python-exceptiongroup propagated-input.
    * gnu/packages/python-xyz.scm (python-cattrs)[propagated-inputs]:
    Drop python-exceptiongroup.
    
    Change-Id: Ib64cfaa4c939b44b4a072c0bd6f45cfdd09aa900
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -21683,7 +21683,6 @@ interfaces.")
     (propagated-inputs
      (list python-attrs
            python-cbor2
-           python-exceptiongroup
            python-orjson
            python-pyyaml
            python-tomlkit
