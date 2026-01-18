58b554e757a550426e8d7169756247ee78acf5bf
Nicolas Graves <ngraves@ngraves.fr>
2026-01-18 22:49:08 +0100

    gnu: python-twisted: Propagate python:tk.
    * gnu/packages/python-xyz.scm (python-twisted)[propagated-inputs]: Add
    python:tk.
    
    Change-Id: I20376d69683d2ba312a565c930639f401f73ab19
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -38168,6 +38168,7 @@ their files and supports any packaging format (including wheels).")
            python-typing-extensions
            python-zope-interface
            ;; [conch]
+           (list python "tk")
            python-appdirs
            python-bcrypt
            python-cryptography
