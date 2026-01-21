86b96f677bca777edac28ee13aadc690b81195c3
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-01-21 14:17:32 +0000

    gnu: Remove python-consul.
    * gnu/packages/python-xyz.scm (python-consul): Delete variable.
    
    Change-Id: Ia3975aa86ad981f607074ac941a405ee9d1f15cf

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -23002,8 +23002,6 @@ natural language processing libraries.")
 discovery, monitoring and configuration.")
     (license license:expat)))
 
-(define-deprecated-package python-consul python-py-consul)
-
 (define-public python-schematics
   (package
     (name "python-schematics")
