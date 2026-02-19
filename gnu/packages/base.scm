7d15fbfb30fbdbda6b2e57b7cd9dfc5260d68052
Nicolas Graves <ngraves@ngraves.fr>
2026-02-19 08:03:15 +0100

    gnu: gnu-make: Improve style.
    * gnu/packages/base.scm (gnu-make)[arguments]: Improve style.
    <#:phases>: Use search-input-file instead of labels, use
    G-expressions.
    
    Change-Id: Ic2cacf099888d59fecee5278d6e59cb749602636
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/base.scm
+++ gnu/packages/base.scm
@@ -633,20 +633,21 @@ standard.")
    (inputs (list guile-3.0))
    (outputs '("out" "debug"))
    (arguments
-    `(,@(if (target-hurd?)
-            '(#:configure-flags '("CFLAGS=-D__alloca=alloca"
-                                  "ac_cv_func_posix_spawn=no"))
-            '())
+    (append
+     (if (target-hurd?)
+         (list #:configure-flags '("CFLAGS=-D__alloca=alloca"
+                                   "ac_cv_func_posix_spawn=no"))
+         (list))
+     (list
       #:phases
-      (modify-phases %standard-phases
-        (add-before 'build 'set-default-shell
-          (lambda* (#:key inputs #:allow-other-keys)
-            ;; Change the default shell from /bin/sh.
-            (let ((bash (assoc-ref inputs "bash")))
+      #~(modify-phases %standard-phases
+          (add-before 'build 'set-default-shell
+            (lambda* (#:key inputs #:allow-other-keys)
+              ;; Change the default shell from /bin/sh.
               (substitute* "src/job.c"
                 (("default_shell =.*$")
-                 (format #f "default_shell = \"~a/bin/sh\";\n"
-                         bash)))))))))
+                 (format #f "default_shell = \"~a\";\n"
+                         (search-input-file inputs "/bin/sh"))))))))))
    (synopsis "Remake files automatically")
    (description
     "Make is a program that is used to control the production of
