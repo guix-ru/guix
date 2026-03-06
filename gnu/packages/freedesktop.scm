guix-ru/guix

commit a17a5c02a8e3278c4bd0a326127d4043e663560e

gnu: xdg-desktop-portal-gtk: Update to 1.15.3.
* gnu/packages/freedesktop.scm (xdg-desktop-portal-gtk)[build-system]: Replace
glib-or-gtk-build-system by meson-build-system.
<#:configure-flags>: Fix for meson build system.  Add lockdown support.
Remove screenshot, screencast and background.
[propagated-inputs]: Replace xdg-desktop-portal by xdg-desktop-portal-next.

Fixes: guix/guix#5075
Change-Id: I5ccef20691a561077885e31b31af911c4780019c

gnu/packages/freedesktop.scm
@@ -3312,7 +3312,7 @@ and others.")
 (define-public xdg-desktop-portal-gtk
   (package
     (name "xdg-desktop-portal-gtk")
-    (version "1.14.1")
+    (version "1.15.3")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -3320,8 +3320,8 @@ and others.")
                     version "/xdg-desktop-portal-gtk-" version ".tar.xz"))
               (sha256
                (base32
-                "002p19j1q3fc8x338ndzxnicwframpgafw31lwvv5avy329akqiy"))))
-    (build-system glib-or-gtk-build-system)
+                "0drvlanj4pydcmq1fhk8nbj5mb2zpf2pxcqxd4g61a0r4hyp98s7"))))
+    (build-system meson-build-system)
     (arguments
      `(#:phases
        (modify-phases %standard-phases
@@ -3332,15 +3332,12 @@ and others.")
                          (chmod po #o666))
                        (find-files "po" "\\.po$"))
              #t)))
-       ;; Enable Gnome portal backends
        #:configure-flags
        (list
-        "--enable-appchooser"
-        "--enable-wallpaper"
-        "--enable-screenshot"
-        "--enable-screencast"
-        "--enable-background"
-        "--enable-settings")))
+        "-Dappchooser=enabled"
+        "-Dwallpaper=enabled"
+        "-Dsettings=enabled"
+        "-Dlockdown=enabled")))
     (native-inputs
      `(("pkg-config" ,pkg-config)
        ("autoconf" ,autoconf)
@@ -3357,7 +3354,7 @@ and others.")
        ("gnome-desktop" ,gnome-desktop)
        ("gsettings-desktop-schemas" ,gsettings-desktop-schemas)))
     (propagated-inputs
-     (list xdg-desktop-portal))
+     (list xdg-desktop-portal-next))
     (home-page "https://github.com/flatpak/xdg-desktop-portal-gtk")
     (synopsis "GTK implementation of xdg-desktop-portal")
     (description

commit 032095cdfbedb79f583c5a1caf43b854145a4a1e

gnu: xdg-desktop-portal-gtk: Improve package definition.
This is a follow-up to a17a5c02a8e3278c4bd0a326127d4043e663560e, which
inadvertently left old inputs and phases.

* gnu/packages/freedesktop.scm (xdg-desktop-portal-gtk)[arguments]:
Add #:glib-or-gtk?.  Remove #:configure-flags and #:phases.
[native-inputs]: Remove autoconf, automake, libtool, libxml2 and which.
Drop labels.
[inputs]: Replace gtk+ with gtk.  Drop labels.

Change-Id: Ifaa7092872884b74fd3f783b677449935bfd4a2a

gnu/packages/freedesktop.scm
@@ -3293,39 +3293,10 @@ and others.")
                (base32
                 "0drvlanj4pydcmq1fhk8nbj5mb2zpf2pxcqxd4g61a0r4hyp98s7"))))
     (build-system meson-build-system)
-    (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         (add-after 'unpack 'po-chmod
-           (lambda _
-             ;; Make sure 'msgmerge' can modify the PO files.
-             (for-each (lambda (po)
-                         (chmod po #o666))
-                       (find-files "po" "\\.po$"))
-             #t)))
-       #:configure-flags
-       (list
-        "-Dappchooser=enabled"
-        "-Dwallpaper=enabled"
-        "-Dsettings=enabled"
-        "-Dlockdown=enabled")))
-    (native-inputs
-     `(("pkg-config" ,pkg-config)
-       ("autoconf" ,autoconf)
-       ("automake" ,automake)
-       ("libtool" ,libtool)
-       ("libxml2" ,libxml2)
-       ("glib:bin" ,glib "bin")
-       ("which" ,which)
-       ("gettext" ,gettext-minimal)))
-    (inputs
-     `(("glib" ,glib)
-       ("gtk" ,gtk+)
-       ("fontconfig" ,fontconfig)
-       ("gnome-desktop" ,gnome-desktop)
-       ("gsettings-desktop-schemas" ,gsettings-desktop-schemas)))
-    (propagated-inputs
-     (list xdg-desktop-portal))
+    (arguments (list #:glib-or-gtk? #t))
+    (native-inputs (list gettext-minimal `(,glib "bin") pkg-config))
+    (inputs (list glib gtk fontconfig gnome-desktop gsettings-desktop-schemas))
+    (propagated-inputs (list xdg-desktop-portal))
     (home-page "https://github.com/flatpak/xdg-desktop-portal-gtk")
     (synopsis "GTK implementation of xdg-desktop-portal")
     (description