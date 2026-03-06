guix-ru/guix

commit 7d04e79c70c484486c99d519d79d7df7b6c86c08

gnu: xdg-desktop-portal: Delete version 1.18.4.
* gnu/packages/freedesktop.scm (xdg-desktop-portal): Delete variable.
(xdg-desktop-portal-next): Rename to ...
(xdg-desktop-portal): ...this.
(xdg-desktop-portal-next): New deprecation alias.
(xdg-desktop-portal-gtk)[propagated-inputs]: Replace xdg-desktop-portal-next
by xdg-desktop-portal.
* gnu/packages/telegram.scm (telegram-desktop)[propagated-inputs]: Replace
xdg-desktop-portal-next by xdg-desktop-portal.
* gnu/packages/patches/xdg-desktop-portal-disable-configuration-search-exit.patch:
Delete file.
* gnu/packages/patches/xdg-desktop-portal-disable-portal-tests.patch: Delete
file.
* gnu/local.mk (dist_patch_DATA): Remove references to those.

Change-Id: I4317d2228002c1c6c65a9fab5aa502e905e4ec3e

gnu/packages/freedesktop.scm
@@ -3204,7 +3204,7 @@ compatible with the well-known scripts of the same name.")
 (define-public xdg-desktop-portal
   (package
     (name "xdg-desktop-portal")
-    (version "1.18.4")
+    (version "1.20.3")
     (source
      (origin
        (method url-fetch)
@@ -3213,15 +3213,19 @@ compatible with the well-known scripts of the same name.")
              version "/xdg-desktop-portal-" version ".tar.xz"))
        (sha256
         (base32
-         "0r8y8qmzcfj7b7brqcxr9lg8pavfds815ffvj0kqc378fhgaln5q"))
+         "1p4yvbhqr8yf231gm69vdz3h7na8m6x1mhiw3bmhg4gm6x4idysb"))
        (patches (search-patches
-                 ;; Disable portal tests since they try to use fuse.
-                 "xdg-desktop-portal-disable-portal-tests.patch"
-                 "xdg-desktop-portal-disable-configuration-search-exit.patch"))))
+                 "xdg-desktop-portal-1.20.3-disable-configuration-search-exit.patch"))
+       (modules '((guix build utils)))
+       ;; Disable failing tests.
+       (snippet #~(substitute* "tests/meson.build"
+                    ((".*test_dynamiclauncher.*") "")
+                    ((".*test_notification.*") "")
+                    ((".*test_usb.*") "")))))
     (build-system meson-build-system)
     (arguments
      `(#:configure-flags
-       (list "-Dsystemd=disabled")
+       (list "-Dsystemd=disabled" "-Ddocumentation=disabled")
        #:phases
        (modify-phases %standard-phases
          (add-after 'unpack 'po-chmod
@@ -3253,9 +3257,13 @@ compatible with the well-known scripts of the same name.")
            gdk-pixbuf
            geoclue
            glib
+           gstreamer
+           gst-plugins-base
+           gst-plugins-good
            json-glib
            libportal
-           pipewire))
+           pipewire
+           umockdev))
     (native-search-paths
      (list (search-path-specification
             (variable "XDG_DESKTOP_PORTAL_DIR")
@@ -3274,41 +3282,10 @@ The portal interfaces include APIs for file access, opening URIs, printing
 and others.")
     (license license:lgpl2.1+)))
 
-(define-public xdg-desktop-portal-next
-  (let ((base xdg-desktop-portal))
-    (package
-      (inherit base)
-      (version "1.20.3")
-      (source
-       (origin
-         (method url-fetch)
-         (uri (string-append
-               "https://github.com/flatpak/xdg-desktop-portal/releases/download/"
-               version "/xdg-desktop-portal-" version ".tar.xz"))
-         (sha256
-          (base32
-           "1p4yvbhqr8yf231gm69vdz3h7na8m6x1mhiw3bmhg4gm6x4idysb"))
-         (patches (search-patches
-                   "xdg-desktop-portal-1.20.3-disable-configuration-search-exit.patch"))
-         (modules '((guix build utils)))
-         ;; Disable failing tests.
-         (snippet #~(substitute* "tests/meson.build"
-                      ((".*test_dynamiclauncher.*") "")
-                      ((".*test_notification.*") "")
-                      ((".*test_usb.*") "")))))
-      (arguments
-       (substitute-keyword-arguments (package-arguments base)
-         ((#:configure-flags flags #~'())
-          ;; Requires python-furo, which isn't packaged for guix, and depends
-          ;; on node.js.
-          #~(append '("-Ddocumentation=disabled")
-                    #$flags))))
-      (inputs (modify-inputs (package-inputs base)
-                (append gstreamer
-                        gst-plugins-base
-                        gst-plugins-good
-                        umockdev))))))
-
+;; Deprecation added on 2026-03-01.
+(define-deprecated-package xdg-desktop-portal-next
+  xdg-desktop-portal)
+  
 (define-public xdg-desktop-portal-gtk
   (package
     (name "xdg-desktop-portal-gtk")
@@ -3354,7 +3331,7 @@ and others.")
        ("gnome-desktop" ,gnome-desktop)
        ("gsettings-desktop-schemas" ,gsettings-desktop-schemas)))
     (propagated-inputs
-     (list xdg-desktop-portal-next))
+     (list xdg-desktop-portal))
     (home-page "https://github.com/flatpak/xdg-desktop-portal-gtk")
     (synopsis "GTK implementation of xdg-desktop-portal")
     (description

gnu/local.mk
@@ -2585,8 +2585,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/x2goclient-fix-makefile.patch            \
   %D%/packages/patches/x265-arm-flags.patch			\
   %D%/packages/patches/x265-4-arm-flags.patch			\
-  %D%/packages/patches/xdg-desktop-portal-disable-portal-tests.patch\
-  %D%/packages/patches/xdg-desktop-portal-disable-configuration-search-exit.patch\
   %D%/packages/patches/xdg-desktop-portal-1.20.3-disable-configuration-search-exit.patch\
   %D%/packages/patches/xdg-desktop-portal-wlr-harcoded-length.patch\
   %D%/packages/patches/xf86-video-siliconmotion-fix-ftbfs.patch \

gnu/packages/patches/xdg-desktop-portal-disable-configuration-search-exit.patch
@@ -1,27 +1,0 @@
-From 37003d1f99246c88cbe7cf2f83616642e0fdf3fb Mon Sep 17 00:00:00 2001
-From: Rodion Goritskov <rodion.goritskov@gmail.com>
-Date: Fri, 27 Sep 2024 00:44:07 +0400
-Subject: [PATCH] portal-impl: Check if there are any configuration files in XDG_DESKTOP_PORTAL_DIR before exiting configuration search
-
----
- src/portal-impl.c | 5 ++---
- 1 file changed, 2 insertions(+), 3 deletions(-)
-
-diff --git a/src/portal-impl.c b/src/portal-impl.c
-index 8c1fd53..8708409 100644
---- a/src/portal-impl.c
-+++ b/src/portal-impl.c
-@@ -438,9 +438,8 @@ load_portal_configuration (gboolean opt_verbose)
- 
-   if (portal_dir != NULL)
-     {
--      load_config_directory (portal_dir, desktops, opt_verbose);
--      /* All other config directories are ignored when this is set */
--      return;
-+      if (load_config_directory (portal_dir, desktops, opt_verbose))
-+	return;
-     }
- 
-   /* $XDG_CONFIG_HOME/xdg-desktop-portal/(DESKTOP-)portals.conf */
--- 
-2.46.0

gnu/packages/patches/xdg-desktop-portal-disable-portal-tests.patch
@@ -1,56 +1,0 @@
-From b7750fa99c31c5f28ac2053fa154bef4038b5cc0 Mon Sep 17 00:00:00 2001
-From: dan <i@dan.games>
-Date: Tue, 2 Jul 2024 19:37:38 +0800
-Subject: [PATCH] tests: remove portal tests.
-
----
- tests/meson.build | 24 ------------------------
- 1 file changed, 24 deletions(-)
-
-diff --git a/tests/meson.build b/tests/meson.build
-index a2dafee..bcd87be 100644
---- a/tests/meson.build
-+++ b/tests/meson.build
-@@ -181,18 +181,6 @@ portal_tests = [
- 
- test_env = env_tests
- test_env.set('XDG_CURRENT_DESKTOP', 'test')
--foreach p : portal_tests
--    test(
--      'test-portals-@0@'.format(p),
--      test_portals,
--      args: ['--verbose', '--keep-going', '--tap', '-p', '/portal/@0@'.format(p)],
--      depends: [test_backends, test_portals],
--      env: test_env,
--      is_parallel: false,
--      protocol: test_protocol,
--      suite: 'portals',
--    )
--endforeach
- 
- # Split the portal tests into one test per portal, this makes debugging a lot
- # easier.
-@@ -204,18 +192,6 @@ portal_limited = [
- 
- limited_env = env_tests
- limited_env.set('XDG_CURRENT_DESKTOP', 'limited')
--foreach p : portal_limited
--    test(
--      'limited-portals-@0@'.format(p),
--      limited_portals,
--      args: ['--verbose', '--keep-going', '--tap', '-p', '/limited/@0@'.format(p)],
--      depends: [test_backends, limited_portals],
--      env: limited_env,
--      is_parallel: false,
--      protocol: test_protocol,
--      suite: 'portals',
--    )
--endforeach
- 
- if enable_installed_tests
-   install_data(
-
-base-commit: 11c8a96b147aeae70e3f770313f93b367d53fedd
--- 
-2.41.0
-

gnu/packages/telegram.scm
@@ -609,7 +609,7 @@ support for high performance Telegram Bot creation.")
            `(,gtk+ "bin")
            pkg-config
            python-wrapper
-           xdg-desktop-portal-next))
+           xdg-desktop-portal))
     (inputs
      (list abseil-cpp-cxxstd17
            alsa-lib

commit 9fcda9b44cab266cab7ef204003a3be425450481

gnu: Improve xdg-desktop-portal.
This is a follow-up to 7d04e79c70c484486c99d519d79d7df7b6c86c08, which
inadvertently left out some long-known changes to xdg-desktop-portal.

* gnu/packages/freedesktop.scm (xdg-desktop-portal)[source]<snippet>: Remove.
[arguments]: Convert to list of G-Expressions
<#:phases>: Delete ‘po-chmod’ and ‘relax-gcc-14-strictness’.
Add ‘patch-libumockdev-preload.so-file-name’ and ‘prepare-for-tests’.
[inputs]: Remove gstreamer.  Move gst-plugins-good from here…
[native-inputs]: … to here.  Add umockdev.
(xdg-desktop-portal-next): Drop gratuitous whitespace.

Change-Id: I956df6f52600e7301ba2776c7508488bed92d9ec

gnu/packages/freedesktop.scm
@@ -3216,39 +3216,39 @@ compatible with the well-known scripts of the same name.")
         (base32
          "1p4yvbhqr8yf231gm69vdz3h7na8m6x1mhiw3bmhg4gm6x4idysb"))
        (patches (search-patches
-                 "xdg-desktop-portal-1.20.3-disable-configuration-search-exit.patch"))
-       (modules '((guix build utils)))
-       ;; Disable failing tests.
-       (snippet #~(substitute* "tests/meson.build"
-                    ((".*test_dynamiclauncher.*") "")
-                    ((".*test_notification.*") "")
-                    ((".*test_usb.*") "")))))
+                 "xdg-desktop-portal-1.20.3-disable-configuration-search-exit.patch"))))
     (build-system meson-build-system)
     (arguments
-     `(#:configure-flags
-       (list "-Dsystemd=disabled" "-Ddocumentation=disabled")
-       #:phases
-       (modify-phases %standard-phases
-         (add-after 'unpack 'po-chmod
-           (lambda _
-             ;; Make sure 'msgmerge' can modify the PO files.
-             (for-each (lambda (po)
-                         (chmod po #o666))
-                       (find-files "po" "\\.po$"))))
-         (add-before 'configure 'relax-gcc-14-strictness
-           (lambda _
-             (setenv "CFLAGS"
-                     "-g -O2 -Wno-error=incompatible-pointer-types")))
-         (add-after 'unpack 'set-home-directory
-           (lambda _ (setenv "HOME" "/tmp"))))))
+     (list
+      #:configure-flags #~(list "-Dsystemd=disabled")
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'patch-libumockdev-preload.so-file-name
+            (lambda* (#:key native-inputs inputs #:allow-other-keys)
+              (substitute* "tests/conftest.py"
+                (("libumockdev-preload.so")
+                 (search-input-file (or native-inputs inputs)
+                                    "lib/libumockdev-preload.so")))))
+          (add-after 'unpack 'set-home-directory
+            (lambda _ (setenv "HOME" "/tmp")))
+          (add-before 'check 'prepare-for-tests
+            (lambda _
+              ;; These environment variables must be set when running the
+              ;; tests in an unprivileged container.  It avoids using
+              ;; bubblewrap to validate icon and sound files, which would fail
+              ;; in such an environment.
+              (setenv "XDP_VALIDATE_ICON_INSECURE" "1")
+              (setenv "XDP_VALIDATE_SOUND_INSECURE" "1"))))))
     (native-inputs
      (list gettext-minimal
            `(,glib "bin")
+           gst-plugins-good             ;for wavparse plugin
            pkg-config
            python
            python-dbusmock
            python-pytest
-           python-pytest-xdist))
+           python-pytest-xdist
+           umockdev))
     (inputs
      (list bubblewrap
            dbus
@@ -3258,9 +3258,7 @@ compatible with the well-known scripts of the same name.")
            gdk-pixbuf
            geoclue
            glib
-           gstreamer
            gst-plugins-base
-           gst-plugins-good
            json-glib
            libportal
            pipewire
@@ -3286,7 +3284,7 @@ and others.")
 ;; Deprecation added on 2026-03-01.
 (define-deprecated-package xdg-desktop-portal-next
   xdg-desktop-portal)
-  
+
 (define-public xdg-desktop-portal-gtk
   (package
     (name "xdg-desktop-portal-gtk")