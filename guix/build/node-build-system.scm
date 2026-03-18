df5313ba673f87ceb567c266bc1d175ea822d07a
Nicolas Graves <ngraves@ngraves.fr>
2026-03-18 07:55:37 +0100

    guix: Refactor (guix build json-utils) from the node build system.
    * guix/build/node-build-system.scm
    (delete-fields, modify-json, modify-json-fields, replace-fields)
    (with-atomic-json-file-replacement, add-fields): Move procedures from here...
    
    * guix/build/json-utils.scm: ...to this new file.
    
    * guix/build-system/node.scm
    (%node-build-system-modules, node-build): Add (guix build json-utils)
    module.
    
    * guix/build/tree-sitter-build-system.scm (patch-dependencies): Import
    with-atomic-json-file-replacement from (guix build json-utils).
    
    * guix/import/npm-binary.scm: Import (guix build json-utils).
    
    * CODEOWNERS, etc/teams.scm: Add guix/build/json-utils.scm to the javascript team
    scope.
    
    * Makefile.am: Add guix/build/json-utils.scm.
    
    Change-Id: I5264a5265e376902c0a309c0177b7496ac3b6295
    Signed-off-by: Jelle Licht <jlicht@fsfe.org>

--- guix/build/node-build-system.scm
+++ guix/build/node-build-system.scm
@@ -25,6 +25,7 @@
 
 (define-module (guix build node-build-system)
   #:use-module ((guix build gnu-build-system) #:prefix gnu:)
+  #:use-module (guix build json-utils)
   #:use-module (guix build utils)
   #:use-module (ice-9 format)
   #:use-module (ice-9 ftw)
@@ -39,42 +40,11 @@
             delete-dependencies/except
             delete-dev-dependencies
             delete-dev-dependencies/except
-            delete-fields
-            add-fields
-            modify-json
-            modify-json-fields
-            node-build
-            replace-fields
-            with-atomic-json-file-replacement))
+            node-build))
 
 ;;;
-;;; package.json modification procedures
+;;; Helpers
 ;;;
-
-(define* (with-atomic-json-file-replacement proc
-                                            #:optional (file "package.json"))
-  "Like 'with-atomic-file-replacement', but PROC is called with a single
-argument---the result of parsing FILE's contents as JSON---and should produce
-a value to be written as JSON to the replacement FILE."
-  (with-atomic-file-replacement file
-    (lambda (in out)
-      (scm->json (proc (json->scm in #:ordered #t)) out #:pretty #t))))
-
-(define* (modify-json #:key (file "package.json") #:rest all-arguments)
-  "Provide package.json modifying callbacks such as (delete-dependencies ...)"
-  (let ((modifications
-         (let loop ((arguments all-arguments))
-           (cond
-            ((null? arguments) '())
-            ((keyword? (car arguments)) (loop (cddr arguments)))
-            (else (cons (car arguments) (loop (cdr arguments))))))))
-    (with-atomic-json-file-replacement
-     (lambda (package)
-       (fold (lambda (modification package)
-               (modification package))
-             package
-             modifications))
-     file)))
 
 (define %dependency-keys
   '("devDependencies"
@@ -124,97 +94,6 @@ listed in DEPENDENCIES-TO-PRESERVE."
 dependencies."
   (delete-dependencies/except dependencies-to-preserve
                               #:dependency-keys %dev-dependency-keys))
-
-(define* (modify-json-fields fields field-modifier
-                             #:key
-                             (field-path-mapper identity)
-                             (insert? #f)
-                             (strict? #t))
-  "Return a procedure to supply to `modify-json' which modifies the specified
-JSON file.  FIELDS is a list procedure-specific data structures which should
-include the definition of a ``field-path'' in one of two syntaxes: dot-syntax
-string such as @code{\"devDependencies.esbuild\"}, or a list of strings such
-as @code{(list \"devDependencies\" \"esbuild\")}.
-
-FIELD-MODIFIER is a procedure called with three arguments: 1) the original
-field-path, e.g. \"dependencies.typescript\", 2) the field's
-surrounding (parent) JSON data, as an association list, and 3) the field
-name (key), e.g. \"typescript\".  The value it returns should be the modified
-JSON data associated with the field; in other words, returning the second
-argument without changing it is a no-op.
-
-FIELD-PATH-MAPPER is a procedure which instructs where the field-path is
-located within the field structure.  INSERT? allows the creation of the field
-and any missing intermediate fields, while STRICT? causes an error to be
-thrown if the exact field-path is not found in the data."
-  (lambda (package)
-    (fold
-     (lambda (field package)
-       (let* ((field-path (field-path-mapper field))
-              (field-path (cond
-                           ((string? field-path)
-                            (string-split field-path #\.))
-                           ((and (list? field-path) (every string? field-path))
-                            field-path)
-                           (else (error (format #f "\
-invalid field value provided, expected string or list of strings, got ~s~%"
-                                                field-path))))))
-         (let loop ((data package)
-                    (field-path field-path))
-           (let* ((key (car field-path))
-                  (field-missing? (not (assoc key data)))
-                  (data (if (and field-missing? insert?)
-                            (acons key '() data)
-                            data)))
-             (if field-missing?
-                 (if strict?
-                     (error (format #f "key ~s was not found in data: ~y~%"
-                                    key data))
-                     data)
-                 (if (= (length field-path) 1)
-                     (field-modifier field data key)
-                     (assoc-set! data key
-                                 (loop (assoc-ref data key)
-                                       (cdr field-path)))))))))
-     package
-     fields)))
-
-(define* (delete-fields fields #:key (strict? #t))
-  "Provides a lambda to supply to modify-json which deletes the specified
- `fields` which is a list of field-paths as mentioned in `modify-json-fields`.
- Examples:
-  (delete-fields '(
-    (\"path\" \"to\" \"field\")
-    \"path.to.other.field\"))"
-  (modify-json-fields
-   fields
-   (lambda (_ data key)
-     (format #t "deleting field ~s, of value: ~y~%"
-             key (assoc-ref data key))
-     (assoc-remove! data key))
-   #:strict? strict?))
-
-(define* (replace-fields fields #:key (strict? #t) insert?)
-  "Provides a lambda to supply to modify-json which replaces the value of the
- supplied field. `fields` is a list of pairs, where the first element is the
- field-path and the second element is the value to replace the target with.
- Examples:
-  (replace-fields '(
-    ((\"path\" \"to\" \"field\") \"new field value\")
-    (\"path.to.other.field\" \"new field value\")))"
-  (modify-json-fields
-   fields
-   (lambda (field data key)
-     (let ((value (cdr field)))
-       (format #t "setting field ~s to value: ~y~%" key value)
-       (assoc-set! data key value)))
-   #:field-path-mapper (lambda (field) (car field))
-   #:insert? insert?
-   #:strict? strict?))
-
-(define* (add-fields fields)
-  "Like `replace-fields', but can insert new fields as well."
-  (replace-fields fields #:insert? #t))
 
 (define (delete-dev-dependencies)
   (delete-fields (list "devDependencies" "peerDependencies")
