00a132ca3e9f54c5fbd526f06d43d58fbcd3e1e0
Yelninei <yelninei@tutamail.com>
2026-05-07 12:44:02 +0000

    gnu: glibc: Remove glibc-hurd-pthread-setcancelstate.patch
    IT fails to compile with glibnc 2.43:
    /tmp/guix-build-glibc-intermediate-2.43.drv-0/build/stdlib/fmtmsg.o
    fmtmsg.c: In function 'fmtmsg':
    fmtmsg.c:127:3: error: implicit declaration of function '__libc_ptf_call'; did you mean '__libc_fatal'? [-Wimplicit-function-declaration]
      127 |   __libc_ptf_call (__pthread_setcancelstate,
          |   ^~~~~~~~~~~~~~~
          |   __libc_fatal
    
    *  gnu/packages/base.scm (%glibc-patches): Remove
    glibc-hurd-pthread-setcancelstate.patch
    * gnu/packages/patches/glibc-hurd-pthread_setcancelstate.patch: Remove patch.
    * gnu/local.mk (dist_patch_DATA): Deregister it.
    
    Change-Id: Ib19c2275484bd69433e6ade9a0c2c1b9c1c71099

--- gnu/local.mk
+++ gnu/local.mk
@@ -1507,7 +1507,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/glibc-hurd-getauxval.patch		\
   %D%/packages/patches/glibc-hurd-gettyent.patch		\
   %D%/packages/patches/glibc-hurd-mach-print.patch		\
-  %D%/packages/patches/glibc-hurd-pthread_setcancelstate.patch	\
   %D%/packages/patches/glibc-hurd-signal-sa-siginfo.patch	\
   %D%/packages/patches/glibc-hurd64-gcc-14.2-tls-bug.patch	\
   %D%/packages/patches/glibc-ldd-powerpc.patch			\
