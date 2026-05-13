a3a264700b6daf5e11022855fbb8a5226e1d21e1
Jelle Licht <jlicht@fsfe.org>
2026-05-13 14:07:36 +0200

    gnu: node-lts: Update to 24.15.0.
    * gnu/packages/node.scm (node-lts): Update to 24.15.0.
    
    Change-Id: Id4b151d8eaa31a348f3f9e64db8e912b5eb9f27c

--- gnu/packages/node.scm
+++ gnu/packages/node.scm
@@ -748,14 +748,14 @@ source files.")
 (define-public node-lts
   (package
     (inherit node-bootstrap)
-    (version "24.14.1")
+    (version "24.15.0")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://nodejs.org/dist/v" version
                                   "/node-v" version ".tar.gz"))
               (sha256
                (base32
-                "1cri5lp3a9jhh34dbd48hq2zyb7xjb9qs6zl36l3q2blawgwz642"))
+                "1ndw65avp1pffgcb4wi7r953shd5av8iqan3lsiyawi8vnaf97bj"))
               (modules '((guix build utils)))
               (snippet
                '(begin
