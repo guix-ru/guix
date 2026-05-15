fe649d1769dedb855d8d1707a7467d23bd1f668b
Olivier Farges <olivier.farges@univ-lorraine.fr>
2026-05-15 11:23:03 +0200

    gnu: gnuplot: fix texdir installation path for lua tikz terminal.
    * gnu/packages/gnuplot.scm (gnuplot)[arguments]: Install lua tikz
      files under share/texmf-dist/tex/latex/gnuplot instead of
      texmf-local/tex/latex/gnuplot, so that texlive-libkpathsea can
      locate gnuplot-lua-tikz.sty and gnuplot.lua via GUIX_TEXMF.
    
    Merges: #8635
    Change-Id: Id60e1937160fbdc5d20431c9b6432a9a7da49762
    Signed-off-by: Nicolas Goaziou <mail@nicolasgoaziou.fr>

--- gnu/packages/maths.scm
+++ gnu/packages/maths.scm
@@ -1727,7 +1727,7 @@ in the terminal or with an external viewer.")
      (list #:configure-flags
            #~(list "--with-qt=no"
                    (string-append "--with-texdir=" #$output
-                                  "/texmf-local/tex/latex/gnuplot"))
+                                  "/share/texmf-dist/tex/latex/gnuplot"))
            ;; Plot on a dumb terminal during tests.
            #:make-flags #~'("GNUTERM=dumb")))
     (home-page "http://www.gnuplot.info")
