d4eb2748fa1162ec4a06be05eba5a0890e30dc8b
Ricardo Wurmus <rekado@elephly.net>
2026-05-31 12:11:07 +0200

    gnu: r-scdblfinder: Update to 1.26.1.
    * gnu/packages/bioconductor.scm (r-scdblfinder): Update to 1.26.1.
    
    Change-Id: Ic88f50717be71b4c7a5edc67f9a4ca4d8621bbbc

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -13664,13 +13664,13 @@ cell types based on specific research needs.")
 (define-public r-scdblfinder
   (package
     (name "r-scdblfinder")
-    (version "1.26.0")
+    (version "1.26.1")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "scDblFinder" version))
        (sha256
-        (base32 "1hdzipp4ncc474la668yrickazv4apwks3jg7rbsc368c8hxvwfh"))))
+        (base32 "0j1cdbq4nvr6yyxbkm8nhcy948fmcd8lvzbm11fk1v6m9nsdr7i2"))))
     (properties `((upstream-name . "scDblFinder")))
     (build-system r-build-system)
     (propagated-inputs
