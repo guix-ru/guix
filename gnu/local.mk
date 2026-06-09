e3d012b189a33bc5d9ddf7399d144fadc9614627
Morgan Smith <Morgan.J.Smith@outlook.com>
2026-06-08 21:23:40 -0400

    gnu: Emacs: Use a simpler method to reduce closure size.
    Before and after this change, the closure size of emacs-minimal is 284.0 MiB.
    Removing the patches without adjusting RUN_TEMACS raises the closure size to 830.1 MiB.
    
    * gnu/packages/patches/emacs-exec-path.patch: Delete file
    * gnu/packages/patches/emacs-next-exec-path.patch: Delete file
    * gnu/local.mk (dist_patch_DATA): Unregister patches.
    * gnu/packages/emacs.scm (emacs-minimal):
    [source]: Don't use the patch.
    [arguments]<#:make-flags>: Use "env -i" to clean the environment.
    (emacs-next-minimal): Adjust accordingly.
    
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>

--- gnu/local.mk
+++ gnu/local.mk
@@ -1255,7 +1255,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/emacs-disable-jit-compilation.patch			\
   %D%/packages/patches/emacs-doc-toc-shell-commands.patch	\
   %D%/packages/patches/emacs-elisp-autofmt-fix-region-send.patch	\
-  %D%/packages/patches/emacs-exec-path.patch			\
   %D%/packages/patches/emacs-fix-scheme-indent-function.patch	\
   %D%/packages/patches/emacs-gnus-alias-reference-signature.patch	\
   %D%/packages/patches/emacs-helpful-fix-signature.patch	\
@@ -1268,7 +1267,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/emacs-native-comp-fix-filenames.patch   \
   %D%/packages/patches/emacs-native-comp-pin-packages.patch   \
   %D%/packages/patches/emacs-next-disable-jit-compilation.patch			\
-  %D%/packages/patches/emacs-next-exec-path.patch   \
   %D%/packages/patches/emacs-next-native-comp-fix-filenames.patch   \
   %D%/packages/patches/emacs-pasp-mode-quote-file-names.patch  \
   %D%/packages/patches/emacs-polymode-fix-lexical-variable-error.patch  \
