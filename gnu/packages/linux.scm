248022311925866b9de1a2a11ee88f7ba4de166f
Vagrant Cascadian <vagrant@debian.org>
2026-06-11 13:40:12 -0700

    gnu: linux-libre 7.0: Update to 7.0.12.
    * gnu/packages/linux.scm (linux-libre-7.0-version): Update to 7.0.12.
    (linux-libre-7.0-pristine-source): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -529,7 +529,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
 
 ;; The current "mainline" kernel.
 
-(define-public linux-libre-7.0-version "7.0.11")
+(define-public linux-libre-7.0-version "7.0.12")
 (define-public linux-libre-7.0-gnu-revision "gnu")
 (define deblob-scripts-7.0
   (linux-libre-deblob-scripts
@@ -539,7 +539,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "0a7572zw5y3k4gs4p3h5w0s9bhd63pdjxz6vxbr96sq6r8b6k02n")))
 (define-public linux-libre-7.0-pristine-source
   (let ((version linux-libre-7.0-version)
-        (hash (base32 "012307ni1v555a1rgzsxsg99pj8fplrghvhw0jk3c4d0vmb86v75")))
+        (hash (base32 "1nk5lans9qg1avmmcwyadfps43d3hyjz9a5gjyvsc77w3sjckvap")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-7.0)))
