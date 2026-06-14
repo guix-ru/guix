b1f5452a0213c0e9af1058a489ea0e01cd4db2f4
Nguyễn Gia Phong <cnx@loang.net>
2026-06-14 20:03:45 +0900

    build: fossil: Indent.
    * guix/build/fossil.scm (fossil-fetch): Fix indentation.

--- guix/build/fossil.scm
+++ guix/build/fossil.scm
@@ -42,23 +42,24 @@ Fossil check-in name.  Return #t on success, #f otherwise."
     (guard (c ((invoke-error? c)
                (report-invoke-error c)
                #f))
-    (invoke fossil-command
-      "tarball" check-in file "--name" name "-R"
-      (case (uri-scheme (string->uri-reference uri))
-        ((file https)                   ;clone the repository first
-         (match-let ((repository (simple-format #f "/tmp/~a.fossil" name))
-                     ((input . output) (pipe)))
-           ;; Trust the TLS certificate of the server,
-           ;; since we'll later verify the tarball's checksum.
-           (display "y" output)
-           (close-port output)
-           (with-input-from-port input
-             (cut invoke fossil-command "clone"
-                  "--no-open" "--once" uri repository))
-           (close-port input)
-           repository))
-        ((ssh)                          ;TODO: authentication for SSH
-         (let ((message (string-append "fetching a Fossil repository through SSH"
-                                       " is not supported: " uri)))
-           (raise (condition (&message (message message))))))
-        ((#f) uri))))))                 ;local file
+      (invoke fossil-command
+        "tarball" check-in file "--name" name "-R"
+        (case (uri-scheme (string->uri-reference uri))
+          ((file https)                 ;clone the repository first
+           (match-let ((repository (simple-format #f "/tmp/~a.fossil" name))
+                       ((input . output) (pipe)))
+             ;; Trust the TLS certificate of the server,
+             ;; since we'll later verify the tarball's checksum.
+             (display "y" output)
+             (close-port output)
+             (with-input-from-port input
+               (cut invoke fossil-command "clone"
+                    "--no-open" "--once" uri repository))
+             (close-port input)
+             repository))
+          ((ssh)                        ;TODO: authentication for SSH
+           (let ((message (string-append "fetching a Fossil repository"
+                                         " through SSH is not supported: "
+                                         uri)))
+             (raise (condition (&message (message message))))))
+          ((#f) uri))))))               ;local file
