guix-ru/guix

commit 26d7eb8a4adafc648ef035e91b6bbc4945d4c962

scripts: substitute: restored nars only appear after verification.
The hash of a nar is only known once 'download-nar' completes.  By that point
the nar may already have been partially-restored to its destination for some
time.  Additionally, after a hash mismatch is detected, 'guix substitute'
leaves the invalid contents at the specified destination.  This means that
untrusted, attacker-controlled contents are present at what at some point may
have been or at some point may become a valid store path.  It is entirely
possible that a user or program won't check that a given store item is valid
before trying to use it (it may be that 'guix gc' was run while an obscured
reference was held).  Let's protect them in that case by ensuring that
unverified contents are kept elsewhere, in a temporary directory.

While implementing this one such program was discovered: tests/substitute.scm.
It actually incorrectly computes the hashes of the nars it supplies to (guix
scripts substitute), computing the hash of the plain contents instead of the
hash of the nar.  These tests nevertheless passed because they didn't check
what (guix scripts substitute) gave as output, instead only checking that the
expected contents were at the specified destination.

* guix/scripts/substitute.scm (call-with-temporary-directory-in): new
  procedure.
  (guix-substitute): use it to create a temporary directory next to the final
  destination to restore the nar inside of.  Once it is fully restored, if the
  hash matches, move it to the final destination, deleting the file currently
  there if it already exists.
* tests/substitute.scm (plain-file-nar-sha256): new procedure.
  (%narinfo, "substitute, narinfo with several URLS" test): use it.

Change-Id: Ifeb9b4c912f9d9b2e7477d237da04e7d5175ff83
Signed-off-by: Ludovic Courtès <ludo@gnu.org>

guix/scripts/substitute.scm
@@ -44,7 +44,7 @@
                            . guix:open-connection-for-uri)))
   #:use-module (guix progress)
   #:use-module ((guix build syscalls)
-                #:select (set-thread-name))
+                #:select (set-thread-name mkdtemp!))
   #:use-module (ice-9 rdelim)
   #:use-module (ice-9 match)
   #:use-module (ice-9 format)
@@ -630,6 +630,26 @@ default value."
   ;; use the current output port instead.
   (make-parameter 4))
 
+;; XXX: copied from (guix utils)
+(define (call-with-temporary-directory-in directory proc)
+  "Call PROC with a name of a temporary directory; close the directory and
+delete it when leaving the dynamic extent of this call."
+  (let* ((template  (string-append directory "/guix-directory-"
+                                   ;; In case the temporary directory may be
+                                   ;; garbage collected, including our pid
+                                   ;; ensures that no live process can mistake
+                                   ;; our freshly-created instance for their
+                                   ;; old deleted one.
+                                   (number->string (getpid))
+                                   ".XXXXXX"))
+         (tmp-dir   (mkdtemp! template)))
+    (dynamic-wind
+      (const #t)
+      (lambda ()
+        (proc tmp-dir))
+      (lambda ()
+        (false-if-exception (delete-file-recursively tmp-dir))))))
+
 (define-command (guix-substitute . args)
   (category internal)
   (synopsis "implement the build daemon's substituter protocol")
@@ -712,15 +732,35 @@ default value."
                       actual-hash
                       cpu-usage
                       (with-cpu-usage-monitoring
-                       (process-substitution
-                        store-path destination
-                        #:cache-urls (substitute-urls)
-                        #:acl (current-acl)
-                        #:deduplicate? deduplicate?
-                        #:print-build-trace?
-                        print-build-trace?
-                        #:fast-decompression?
-                        fast-decompression?))))
+                       ;; Restore inside a temporary directory until the hash
+                       ;; can be verified so that no dangling references a
+                       ;; user may have laying around will point to
+                       ;; attacker-controlled content in the meantime.
+                       (call-with-temporary-directory-in (dirname destination)
+                         (lambda (temp-directory)
+                           (let* ((temp-destination
+                                   (string-append temp-directory "/restored"))
+                                  (narinfo
+                                   expected-hash
+                                   actual-hash
+                                   (process-substitution
+                                    store-path temp-destination
+                                    #:cache-urls (substitute-urls)
+                                    #:acl (current-acl)
+                                    #:deduplicate? deduplicate?
+                                    #:print-build-trace?
+                                    print-build-trace?
+                                    #:fast-decompression?
+                                    fast-decompression?)))
+                             (when (and expected-hash actual-hash
+                                        (bytevector=? actual-hash
+                                                      expected-hash))
+                               (catch 'system-error
+                                 (lambda ()
+                                   (delete-file-recursively destination))
+                                 (const #f))
+                               (rename-file temp-destination destination))
+                             (values narinfo expected-hash actual-hash)))))))
 
                  (if expected-hash
                      (begin
@@ -777,6 +817,7 @@ default value."
 
 ;;; Local Variables:
 ;;; eval: (put 'with-redirected-error-port 'scheme-indent-function 0)
+;;; eval: (put 'call-with-temporary-directory-in 'scheme-indent-function 1)
 ;;; End:
 
 ;;; substitute.scm ends here

tests/substitute.scm
@@ -39,6 +39,7 @@
   #:use-module (rnrs io ports)
   #:use-module (web uri)
   #:use-module (ice-9 regex)
+  #:use-module (ice-9 binary-ports)
   #:use-module (srfi srfi-11)
   #:use-module (srfi srfi-26)
   #:use-module (srfi srfi-34)
@@ -142,6 +143,21 @@ version identifier.."
   ;; <https://www.rfc-editor.org/rfc/rfc5737>.
   "http://203.0.113.1")
 
+(define (plain-file-nar-sha256 string)
+  (sha256 (call-with-output-bytevector
+           (lambda (port)
+             (let ((bv (string->utf8 string)))
+               (call-with-input-bytevector
+                bv
+                (lambda (contents)
+                  (write-file-tree #t port
+                                   #:file-type+size
+                                   (lambda (_)
+                                     (values 'regular
+                                             (bytevector-length
+                                              bv)))
+                                   #:file-port
+                                   (const contents)))))))))
 
 (define %narinfo
   ;; Skeleton of the narinfo used below.
@@ -150,7 +166,7 @@ version identifier.."
 URL: example.nar
 Compression: none
 NarHash: sha256:" (bytevector->nix-base32-string
-                   (sha256 (string->utf8 "Substitutable data."))) "
+                   (plain-file-nar-sha256 "Substitutable data.")) "
 NarSize: 42
 References: bar baz
 Deriver: " (%store-prefix) "/foo.drv
@@ -819,7 +835,7 @@ Compression: lzip
 URL: example.nar
 Compression: none
 NarHash: sha256:" (bytevector->nix-base32-string
-                   (sha256 (string->utf8 "Substitutable data."))) "
+                   (plain-file-nar-sha256 "Substitutable data.")) "
 NarSize: 42
 References: bar baz
 Deriver: " (%store-prefix) "/foo.drv

commit 57fd857c1e3ff11511ba451021d559e874b50e34

scripts: substitute: Accommodate renaming unwritable directories.
This fixes a regression introduced in 26d7eb8a4adafc648ef035e91b6bbc4945d4c962
when running ‘guix-daemon’ without root privileges.

On rootless installations, we must abide by the usual file access rules, which
include the requirement that a directory be writable before it be renamed (the
rationale being that its ".." entry needs to be modified).

* guix/scripts/substitute.scm (rename-file*): New procedure.
(guix-substitute): Use it.
* tests/substitute.scm (directory-nar-sha256): New procedure.
(%narinfo/directory): New variable.
(call-with-narinfo): Add '#:directory?' keyword argument.
(with-directory-narinfo): New syntax.
("substitute, authorized key, directory"): New test case.

Fixes: guix/guix#9686
Change-Id: I9d399dd5f1717b7d02854d97e1100a0a464fac75
Signed-off-by: Ludovic Courtès <ludo@gnu.org>
Merges: #9702

guix/scripts/substitute.scm
@@ -638,6 +638,39 @@ delete it when leaving the dynamic extent of this call."
       (lambda ()
         (false-if-exception (delete-file-recursively tmp-dir))))))
 
+(define (rename-file* from to)
+  "Like `rename-file', but temporarily make FROM user-writable if it is an
+unwritable directory, restoring its original permissions afterward.
+
+This requires that, in the case that FROM is a directory, it is readable."
+  (let ((st (lstat from)))
+    (match (stat:type st)
+      ('directory
+       (catch 'system-error
+         (lambda ()
+           (rename-file from to))
+         (lambda args
+           (cond
+            ((= (system-error-errno args) EACCES)
+             ;; Assume it's because FROM is not writable.  Open FROM
+             ;; so we can use fchmod instead of risking following a symbolic
+             ;; link.
+             (call-with-port (open from (logior
+                                         ;; XXX: requires that FROM is
+                                         ;; readable.
+                                         O_RDONLY
+                                         O_NOFOLLOW
+                                         O_DIRECTORY))
+               (lambda (port)
+                 ;; stat again using fstat this time just in case what we
+                 ;; opened isn't the directory we originally lstat'ed.
+                 (let ((perms (stat:perms (stat port))))
+                   (chmod port (logior perms #o200))
+                   (rename-file from to)
+                   (chmod port perms)))))
+            (else (apply throw args))))))
+      (_ (rename-file from to)))))
+
 (define-command (guix-substitute . args)
   (category internal)
   (synopsis "implement the build daemon's substituter protocol")
@@ -747,7 +780,7 @@ delete it when leaving the dynamic extent of this call."
                                  (lambda ()
                                    (delete-file-recursively destination))
                                  (const #f))
-                               (rename-file temp-destination destination))
+                               (rename-file* temp-destination destination))
                              (values narinfo expected-hash actual-hash)))))))
 
                  (if expected-hash

tests/substitute.scm
@@ -38,6 +38,7 @@
   #:use-module (rnrs bytevectors)
   #:use-module (rnrs io ports)
   #:use-module (web uri)
+  #:use-module (ice-9 match)
   #:use-module (ice-9 regex)
   #:use-module (ice-9 binary-ports)
   #:use-module (srfi srfi-11)
@@ -65,7 +66,7 @@ it writes to GUIX-WARNING-PORT a messages that matches ERROR-RX."
 
 (define (request-substitution item destination)
   "Run 'guix substitute --substitute' to fetch ITEM to DESTINATION."
-  (false-if-exception (delete-file destination))
+  (false-if-exception (delete-file-recursively destination))
   (with-input-from-string (string-append "substitute " item " "
                                          destination "\n")
     (lambda ()
@@ -143,6 +144,29 @@ version identifier.."
   ;; <https://www.rfc-editor.org/rfc/rfc5737>.
   "http://203.0.113.1")
 
+(define (directory-nar-sha256 string)
+  "Compute the sha256 of the nar of a directory containing a single regular
+file named \"foo\" containing STRING."
+  (sha256 (call-with-output-bytevector
+           (lambda (port)
+             (let ((bv (string->utf8 string)))
+               (call-with-input-bytevector
+                bv
+                (lambda (contents)
+                  (write-file-tree "root" port
+                                   #:file-type+size
+                                   (match-lambda
+                                     ("root"
+                                      (values 'directory #f))
+                                     ("root/foo"
+                                      (values 'regular
+                                              (bytevector-length
+                                               bv))))
+                                   #:file-port
+                                   (const contents)
+                                   #:directory-entries
+                                   (const '("foo"))))))))))
+
 (define (plain-file-nar-sha256 string)
   (sha256 (call-with-output-bytevector
            (lambda (port)
@@ -172,9 +196,22 @@ References: aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa-bar bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
 Deriver: cccccccccccccccccccccccccccccccc-foo.drv
 System: mips64el-linux\n"))
 
+(define %narinfo/directory
+  (string-append "StorePath: " (%store-prefix)
+                 "/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa-foo
+URL: example.nar
+Compression: none
+NarHash: sha256:" (bytevector->nix-base32-string
+                   (directory-nar-sha256 "Substitutable data.")) "
+NarSize: 304
+References: aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa-bar bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb-baz
+Deriver: cccccccccccccccccccccccccccccccc-foo.drv
+System: mips64el-linux\n"))
+
 (define* (call-with-narinfo narinfo thunk
                             #:optional
-                            (narinfo-directory %main-substitute-directory))
+                            (narinfo-directory %main-substitute-directory)
+                            #:key directory?)
   "Call THUNK in a context where the directory at URL is populated with
 a file for NARINFO."
   (mkdir-p narinfo-directory)
@@ -195,9 +232,16 @@ a file for NARINFO."
           (cut display narinfo <>))
 
         ;; Prepare the nar.
-        (call-with-output-file
-            (string-append narinfo-directory "/example.out")
-          (cut display "Substitutable data." <>))
+        (let ((out (string-append narinfo-directory "/example.out")))
+          (when (file-exists? out)
+            (delete-file-recursively out))
+          (if directory?
+              (begin
+                (mkdir out)
+                (call-with-output-file (string-append out "/foo")
+                  (cut display "Substitutable data." <>)))
+              (call-with-output-file out
+                (cut display "Substitutable data." <>))))
         (call-with-output-file
             (string-append narinfo-directory "/example.nar")
           (cute write-file
@@ -214,6 +258,9 @@ a file for NARINFO."
 
 (define-syntax-rule (with-narinfo* narinfo directory body ...)
   (call-with-narinfo narinfo (lambda () body ...) directory))
+
+(define-syntax-rule (with-directory-narinfo narinfo body ...)
+  (call-with-narinfo narinfo (lambda () body ...) #:directory? #t))
 
 ;; Transmit these options to 'guix substitute'.
 (substitute-urls (list (getenv "GUIX_BINARY_SUBSTITUTE_URL")))
@@ -523,6 +570,24 @@ System: mips64el-linux\n")))
               (stat:perms (lstat "substitute-retrieved"))))
       (lambda ()
         (false-if-exception (delete-file "substitute-retrieved"))))))
+
+(test-equal "substitute, authorized key, directory"
+  '("Substitutable data." 1 #o555)
+  (with-directory-narinfo (string-append %narinfo/directory "Signature: "
+                                         (signature-field %narinfo/directory))
+    (dynamic-wind
+      (const #t)
+      (lambda ()
+        (request-substitution (string-append (%store-prefix)
+                                             "/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa-foo")
+                              "substitute-retrieved")
+        (list (call-with-input-file "substitute-retrieved/foo" get-string-all)
+              (stat:mtime (lstat "substitute-retrieved"))
+              (stat:perms (lstat "substitute-retrieved"))))
+      (lambda ()
+        ;; Needs to be writable to be deleted
+        (false-if-exception (chmod "substitute-retrieved" #o755))
+        (false-if-exception (delete-file-recursively "substitute-retrieved"))))))
 
 (test-equal "substitute, authorized key, first substitute URL is unroutable"
   '("Substitutable data." 1 #o444)