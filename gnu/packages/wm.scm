3cf19c06b6bba1fe0a8e515c8c19f8ea787c5db3
vicvbcun <guix@ikherbers.com>
2026-07-08 21:10:31 +0200

    gnu: wlroots: Update to 0.20.2.
    * gnu/packages/wm.scm (wlroots): Update to 0.20.2.
    
    Merges: https://codeberg.org/guix/guix/pulls/9821
    Reviewed-by: kestrelwx <kestrel.w@proton.me>
    Signed-off-by: Nguyễn Gia Phong <cnx@loang.net>

--- gnu/packages/wm.scm
+++ gnu/packages/wm.scm
@@ -2884,7 +2884,7 @@ XDG-Output for wlclock to work.")
 (define-public wlroots
   (package
     (name "wlroots")
-    (version "0.20.1")
+    (version "0.20.2")
     (source
      (origin
        (method git-fetch)
@@ -2893,7 +2893,7 @@ XDG-Output for wlclock to work.")
              (commit version)))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0n9wkmqnja5fnwn8an36jqn6nzlb1vjdzi0v932pn5bpgmv3brxs"))))
+        (base32 "15jizs8487pq8mi06rn2jvwds5734dnjnh4yvg8sz9yqzjd35mjm"))))
     (build-system meson-build-system)
     (arguments
      (list #:phases
