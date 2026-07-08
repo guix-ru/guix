aa73e1129cecbab893bce818836065eccbd2d30a
Anderson Torres <anderson.torres.8519@gmail.com>
2026-07-08 18:01:24 -0300

    gnu: wm.scm: New dummy file.
    * gnu/packages/wm.scm: New dummy file.
    
    Change-Id: Ibeb3e2948aeed9db5aa8818d8076b90e09e1160e
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>

---
+++ gnu/packages/wm.scm
@@ -0,0 +1,456 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2026 Anderson Torres <anderson.torres.8519@gmail.com>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+;; This whole file was deprecated at 2026-07-08.
+
+(define-module (gnu packages wm)
+  #:use-module (guix deprecation))
+
+(define-deprecated/public-alias avizo
+  (@ (gnu packages window-management) avizo))
+
+(define-deprecated/public-alias awesome
+  (@ (gnu packages window-management) awesome))
+
+(define-deprecated/public-alias awww
+  (@ (gnu packages window-management) awww))
+
+(define-deprecated/public-alias berry
+  (@ (gnu packages window-management) berry))
+
+(define-deprecated/public-alias bspwm
+  (@ (gnu packages window-management) bspwm))
+
+(define-deprecated/public-alias cage
+  (@ (gnu packages window-management) cage))
+
+(define-deprecated/public-alias cagebreak
+  (@ (gnu packages window-management) cagebreak))
+
+(define-deprecated/public-alias cl-stumpwm
+  (@ (gnu packages window-management) cl-stumpwm))
+
+(define-deprecated/public-alias cwm
+  (@ (gnu packages window-management) cwm))
+
+(define-deprecated/public-alias dank-material-shell
+  (@ (gnu packages window-management) dank-material-shell))
+
+(define-deprecated/public-alias dank-material-shell-minimal
+  (@ (gnu packages window-management) dank-material-shell-minimal))
+
+(define-deprecated/public-alias devour
+  (@ (gnu packages window-management) devour))
+
+(define-deprecated/public-alias dunst
+  (@ (gnu packages window-management) dunst))
+
+(define-deprecated/public-alias dwl
+  (@ (gnu packages window-management) dwl))
+
+(define-deprecated/public-alias evilwm
+  (@ (gnu packages window-management) evilwm))
+
+(define-deprecated/public-alias eww
+  (@ (gnu packages window-management) eww))
+
+(define-deprecated/public-alias fbautostart
+  (@ (gnu packages window-management) fbautostart))
+
+(define-deprecated/public-alias fluxbox
+  (@ (gnu packages window-management) fluxbox))
+
+(define-deprecated/public-alias fnott
+  (@ (gnu packages window-management) fnott))
+
+(define-deprecated/public-alias fvwm3
+  (@ (gnu packages window-management) fvwm3))
+
+(define-deprecated/public-alias ghc-xmobar
+  (@ (gnu packages window-management) ghc-xmobar))
+
+(define-deprecated/public-alias ghc-xmonad-contrib
+  (@ (gnu packages window-management) ghc-xmonad-contrib))
+
+(define-deprecated/public-alias grimblast
+  (@ (gnu packages window-management) grimblast))
+
+(define-deprecated/public-alias grimshot
+  (@ (gnu packages window-management) grimshot))
+
+(define-deprecated/public-alias gtklock
+  (@ (gnu packages window-management) gtklock))
+
+(define-deprecated/public-alias gubar
+  (@ (gnu packages window-management) gubar))
+
+(define-deprecated/public-alias herbstluftwm
+  (@ (gnu packages window-management) herbstluftwm))
+
+(define-deprecated/public-alias hikari
+  (@ (gnu packages window-management) hikari))
+
+(define-deprecated/public-alias hypridle
+  (@ (gnu packages window-management) hypridle))
+
+(define-deprecated/public-alias hyprland
+  (@ (gnu packages window-management) hyprland))
+
+(define-deprecated/public-alias hyprmag
+  (@ (gnu packages window-management) hyprmag))
+
+(define-deprecated/public-alias i3-autotiling
+  (@ (gnu packages window-management) i3-autotiling))
+
+(define-deprecated/public-alias i3-wm
+  (@ (gnu packages window-management) i3-wm))
+
+(define-deprecated/public-alias i3blocks
+  (@ (gnu packages window-management) i3blocks))
+
+(define-deprecated/public-alias i3ipc-glib
+  (@ (gnu packages window-management) i3ipc-glib))
+
+(define-deprecated/public-alias i3lock
+  (@ (gnu packages window-management) i3lock))
+
+(define-deprecated/public-alias i3lock-blur
+  (@ (gnu packages window-management) i3lock-blur))
+
+(define-deprecated/public-alias i3lock-color
+  (@ (gnu packages window-management) i3lock-color))
+
+(define-deprecated/public-alias i3lock-fancy
+  (@ (gnu packages window-management) i3lock-fancy))
+
+(define-deprecated/public-alias i3status
+  (@ (gnu packages window-management) i3status))
+
+(define-deprecated/public-alias icewm
+  (@ (gnu packages window-management) icewm))
+
+(define-deprecated/public-alias idesk
+  (@ (gnu packages window-management) idesk))
+
+(define-deprecated/public-alias jwm
+  (@ (gnu packages window-management) jwm))
+
+(define-deprecated/public-alias kanshi
+  (@ (gnu packages window-management) kanshi))
+
+(define-deprecated/public-alias keybinder
+  (@ (gnu packages window-management) keybinder))
+
+(define-deprecated/public-alias labwc
+  (@ (gnu packages window-management) labwc))
+
+(define-deprecated/public-alias lavalauncher
+  (@ (gnu packages window-management) lavalauncher))
+
+(define-deprecated/public-alias lemonbar
+  (@ (gnu packages window-management) lemonbar))
+
+(define-deprecated/public-alias lemonbar-xft
+  (@ (gnu packages window-management) lemonbar-xft))
+
+(define-deprecated/public-alias libdisplay-info
+  (@ (gnu packages window-management) libdisplay-info))
+
+(define-deprecated/public-alias libucl
+  (@ (gnu packages window-management) libucl))
+
+(define-deprecated/public-alias mako
+  (@ (gnu packages window-management) mako))
+
+(define-deprecated/public-alias mangowm
+  (@ (gnu packages window-management) mangowm))
+
+(define-deprecated/public-alias menumaker
+  (@ (gnu packages window-management) menumaker))
+
+(define-deprecated/public-alias mjwm
+  (@ (gnu packages window-management) mjwm))
+
+(define-deprecated/public-alias niri
+  (@ (gnu packages window-management) niri))
+
+(define-deprecated/public-alias obconf
+  (@ (gnu packages window-management) obconf))
+
+(define-deprecated/public-alias openbox
+  (@ (gnu packages window-management) openbox))
+
+(define-deprecated/public-alias papersway
+  (@ (gnu packages window-management) papersway))
+
+(define-deprecated/public-alias perl-anyevent-i3
+  (@ (gnu packages window-management) perl-anyevent-i3))
+
+(define-deprecated/public-alias polybar
+  (@ (gnu packages window-management) polybar))
+
+(define-deprecated/public-alias polycat
+  (@ (gnu packages window-management) polycat))
+
+(define-deprecated/public-alias python-i3-py
+  (@ (gnu packages window-management) python-i3-py))
+
+(define-deprecated/public-alias python-i3ipc
+  (@ (gnu packages window-management) python-i3ipc))
+
+(define-deprecated/public-alias qtile
+  (@ (gnu packages window-management) qtile))
+
+(define-deprecated/public-alias quickshell
+  (@ (gnu packages window-management) quickshell))
+
+(define-deprecated/public-alias quickswitch-i3
+  (@ (gnu packages window-management) quickswitch-i3))
+
+(define-deprecated/public-alias quicktile
+  (@ (gnu packages window-management) quicktile))
+
+(define-deprecated/public-alias sandbar
+  (@ (gnu packages window-management) sandbar))
+
+(define-deprecated/public-alias sbcl-stumpwm-acpi-backlight
+  (@ (gnu packages window-management) sbcl-stumpwm-acpi-backlight))
+
+(define-deprecated/public-alias sbcl-stumpwm-battery-portable
+  (@ (gnu packages window-management) sbcl-stumpwm-battery-portable))
+
+(define-deprecated/public-alias sbcl-stumpwm-binwarp
+  (@ (gnu packages window-management) sbcl-stumpwm-binwarp))
+
+(define-deprecated/public-alias sbcl-stumpwm-bluetooth
+  (@ (gnu packages window-management) sbcl-stumpwm-bluetooth))
+
+(define-deprecated/public-alias sbcl-stumpwm-cpu
+  (@ (gnu packages window-management) sbcl-stumpwm-cpu))
+
+(define-deprecated/public-alias sbcl-stumpwm-disk
+  (@ (gnu packages window-management) sbcl-stumpwm-disk))
+
+(define-deprecated/public-alias sbcl-stumpwm-globalwindows
+  (@ (gnu packages window-management) sbcl-stumpwm-globalwindows))
+
+(define-deprecated/public-alias sbcl-stumpwm-hostname
+  (@ (gnu packages window-management) sbcl-stumpwm-hostname))
+
+(define-deprecated/public-alias sbcl-stumpwm-kbd-layouts
+  (@ (gnu packages window-management) sbcl-stumpwm-kbd-layouts))
+
+(define-deprecated/public-alias sbcl-stumpwm-mem
+  (@ (gnu packages window-management) sbcl-stumpwm-mem))
+
+(define-deprecated/public-alias sbcl-stumpwm-net
+  (@ (gnu packages window-management) sbcl-stumpwm-net))
+
+(define-deprecated/public-alias sbcl-stumpwm-notify
+  (@ (gnu packages window-management) sbcl-stumpwm-notify))
+
+(define-deprecated/public-alias sbcl-stumpwm-numpad-layouts
+  (@ (gnu packages window-management) sbcl-stumpwm-numpad-layouts))
+
+(define-deprecated/public-alias sbcl-stumpwm-pamixer
+  (@ (gnu packages window-management) sbcl-stumpwm-pamixer))
+
+(define-deprecated/public-alias sbcl-stumpwm-pass
+  (@ (gnu packages window-management) sbcl-stumpwm-pass))
+
+(define-deprecated/public-alias sbcl-stumpwm-rofi
+  (@ (gnu packages window-management) sbcl-stumpwm-rofi))
+
+(define-deprecated/public-alias sbcl-stumpwm-screenshot
+  (@ (gnu packages window-management) sbcl-stumpwm-screenshot))
+
+(define-deprecated/public-alias sbcl-stumpwm-stump-nm
+  (@ (gnu packages window-management) sbcl-stumpwm-stump-nm))
+
+(define-deprecated/public-alias sbcl-stumpwm-stumptray
+  (@ (gnu packages window-management) sbcl-stumpwm-stumptray))
+
+(define-deprecated/public-alias sbcl-stumpwm-swm-gaps
+  (@ (gnu packages window-management) sbcl-stumpwm-swm-gaps))
+
+(define-deprecated/public-alias sbcl-stumpwm-tomato
+  (@ (gnu packages window-management) sbcl-stumpwm-tomato))
+
+(define-deprecated/public-alias sbcl-stumpwm-ttf-fonts
+  (@ (gnu packages window-management) sbcl-stumpwm-ttf-fonts))
+
+(define-deprecated/public-alias sbcl-stumpwm-wifi
+  (@ (gnu packages window-management) sbcl-stumpwm-wifi))
+
+(define-deprecated/public-alias sbcl-stumpwm-winner-mode
+  (@ (gnu packages window-management) sbcl-stumpwm-winner-mode))
+
+(define-deprecated/public-alias sbcl-stumpwm-wpctl
+  (@ (gnu packages window-management) sbcl-stumpwm-wpctl))
+
+(define-deprecated/public-alias scenefx
+  (@ (gnu packages window-management) scenefx))
+
+(define-deprecated/public-alias sdorfehs
+  (@ (gnu packages window-management) sdorfehs))
+
+(define-deprecated/public-alias shaderbg
+  (@ (gnu packages window-management) shaderbg))
+
+(define-deprecated/public-alias sov
+  (@ (gnu packages window-management) sov))
+
+(define-deprecated/public-alias spectrwm
+  (@ (gnu packages window-management) spectrwm))
+
+(define-deprecated/public-alias stumpish
+  (@ (gnu packages window-management) stumpish))
+
+(define-deprecated/public-alias stumpwm
+  (@ (gnu packages window-management) stumpwm))
+
+(define-deprecated/public-alias stumpwm+slynk
+  (@ (gnu packages window-management) stumpwm+slynk))
+
+(define-deprecated/public-alias sway
+  (@ (gnu packages window-management) sway))
+
+(define-deprecated/public-alias sway-audio-idle-inhibit
+  (@ (gnu packages window-management) sway-audio-idle-inhibit))
+
+(define-deprecated/public-alias swaybg
+  (@ (gnu packages window-management) swaybg))
+
+(define-deprecated/public-alias swayfx
+  (@ (gnu packages window-management) swayfx))
+
+(define-deprecated/public-alias swayidle
+  (@ (gnu packages window-management) swayidle))
+
+(define-deprecated/public-alias swaylock
+  (@ (gnu packages window-management) swaylock))
+
+(define-deprecated/public-alias swaylock-effects
+  (@ (gnu packages window-management) swaylock-effects))
+
+(define-deprecated/public-alias swaynotificationcenter
+  (@ (gnu packages window-management) swaynotificationcenter))
+
+(define-deprecated/public-alias swc
+  (@ (gnu packages window-management) swc))
+
+(define-deprecated/public-alias trayer-srg
+  (@ (gnu packages window-management) trayer-srg))
+
+(define-deprecated/public-alias velox
+  (@ (gnu packages window-management) velox))
+
+(define-deprecated/public-alias wawa
+  (@ (gnu packages window-management) wawa))
+
+(define-deprecated/public-alias waybar
+  (@ (gnu packages window-management) waybar))
+
+(define-deprecated/public-alias waybar-cpu-histogram
+  (@ (gnu packages window-management) waybar-cpu-histogram))
+
+(define-deprecated/public-alias waybar-experimental
+  (@ (gnu packages window-management) waybar-experimental))
+
+(define-deprecated/public-alias wbg
+  (@ (gnu packages window-management) wbg))
+
+(define-deprecated/public-alias wdisplays
+  (@ (gnu packages window-management) wdisplays))
+
+(define-deprecated/public-alias wf-config
+  (@ (gnu packages window-management) wf-config))
+
+(define-deprecated/public-alias wideriver
+  (@ (gnu packages window-management) wideriver))
+
+(define-deprecated/public-alias wl-mirror
+  (@ (gnu packages window-management) wl-mirror))
+
+(define-deprecated/public-alias wlclock
+  (@ (gnu packages window-management) wlclock))
+
+(define-deprecated/public-alias wld
+  (@ (gnu packages window-management) wld))
+
+(define-deprecated/public-alias wlogout
+  (@ (gnu packages window-management) wlogout))
+
+(define-deprecated/public-alias wlopm
+  (@ (gnu packages window-management) wlopm))
+
+(define-deprecated/public-alias wlr-randr
+  (@ (gnu packages window-management) wlr-randr))
+
+(define-deprecated/public-alias wlroots
+  (@ (gnu packages window-management) wlroots))
+
+(define-deprecated/public-alias wlroots-0.15
+  (@ (gnu packages window-management) wlroots-0.15))
+
+(define-deprecated/public-alias wlroots-0.17
+  (@ (gnu packages window-management) wlroots-0.17))
+
+(define-deprecated/public-alias wlroots-0.18
+  (@ (gnu packages window-management) wlroots-0.18))
+
+(define-deprecated/public-alias wlroots-0.19
+  (@ (gnu packages window-management) wlroots-0.19))
+
+(define-deprecated/public-alias wlroots-0.20
+  (@ (gnu packages window-management) wlroots-0.20))
+
+(define-deprecated/public-alias wlrsetroot
+  (@ (gnu packages window-management) wlrsetroot))
+
+(define-deprecated/public-alias wmenu
+  (@ (gnu packages window-management) wmenu))
+
+(define-deprecated/public-alias wsbg
+  (@ (gnu packages window-management) wsbg))
+
+(define-deprecated/public-alias xclickroot
+  (@ (gnu packages window-management) xclickroot))
+
+(define-deprecated/public-alias xinitrc-xsession
+  (@ (gnu packages window-management) xinitrc-xsession))
+
+(define-deprecated/public-alias xmenu
+  (@ (gnu packages window-management) xmenu))
+
+(define-deprecated/public-alias xmobar
+  (@ (gnu packages window-management) xmobar))
+
+(define-deprecated/public-alias xmonad
+  (@ (gnu packages window-management) xmonad))
+
+(define-deprecated/public-alias xnotify
+  (@ (gnu packages window-management) xnotify))
+
+(define-deprecated/public-alias yambar-wayland
+  (@ (gnu packages window-management) yambar-wayland))
+
+(define-deprecated/public-alias yeganesh
+  (@ (gnu packages window-management) yeganesh))
+
+;;; wm.scm ends here
