51e278a0cc7efd05124c2bfeaeabb4974f4ef8ae
Julian Flake <flake@uni-koblenz.de>
2026-07-15 09:17:59 +0200

    gnu: Add emacs-cypher-mode.
    * gnu/packages/emacs-xyz.scm (emacs-cypher-mode): New variable.
    
    Change-Id: If588571d896bd1ae6ee791bab6f675153e30e669
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -7398,6 +7398,30 @@ eshell.")
     (home-page "https://repo.or.cz/emacs-capf-autosuggest")
     (license license:gpl3+)))
 
+(define-public emacs-cypher-mode
+  ;; There are no tagged releases upstream.
+  (let ((commit "ce8543d7877c736c574a17b49874c9dcdc7a06d6")
+        (revision "0"))
+  (package
+    (name "emacs-cypher-mode")
+    (version (git-version "0.0.6" revision commit)) ; from cypher-mode.el
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/fxbois/cypher-mode")
+             (commit commit)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0vbcq807jpjssabmyjcdkpp6nnx1288is2c6x79dkrviw2xxw3qf"))))
+    (build-system emacs-build-system)
+    (arguments (list #:tests? #f)) ; no tests
+    (home-page "https://github.com/fxbois/cypher-mode")
+    (synopsis "Major mode for editing cypher scripts")
+    (description "This package provides a major mode for cypher files, which
+are used in the context of graph databases.")
+    (license license:gpl2+))))
+
 (define-public emacs-org-block-capf
   ;; There are no tagged releases upstream
   (let ((commit "080cfd2ed630a6739633b07a8ab6b896a1b5ef4a")
