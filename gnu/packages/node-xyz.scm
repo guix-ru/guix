9d1963efd78416c80395cb8432b055f9d071a769
Maxim Cournoyer <maxim@guixotic.coop>
2026-08-04 11:51:03 +0900

    gnu: node-xyz: Adjust for missing (guix build json-utils) module.
    This is a follow-up to commit b00aa06f870592dea795abedc3d51392688d595f,
    renamed `delete-fields' to `delete-json-fields', but forgot to add (guix build
    json-utils) in the #:modules, which defines it (and is part of the
    node-build-system default imported modules).
    
    * gnu/packages/node-xyz.scm (node-acorn, node-fastest-levenshtein)
    (node-glob, node-lru-cache, node-minimatch, node-minipass-7 node-path-scurry)
    (node-serialport-bindings, node-sqlite3)
    (node-addon-api, (node-source-map): [#:modules]: Add (guix build json-utils).

--- gnu/packages/node-xyz.scm
+++ gnu/packages/node-xyz.scm
@@ -61,6 +61,7 @@
        ((guix build node-build-system)
         (srfi srfi-1)
         (ice-9 match)
+        (guix build json-utils)
         (guix build utils))
        #:phases
        (modify-phases %standard-phases
@@ -124,6 +125,7 @@ architecture supporting plugins.")
        ((guix build node-build-system)
         (srfi srfi-1)
         (ice-9 match)
+        (guix build json-utils)
         (guix build utils))
        #:phases
        (modify-phases %standard-phases
@@ -1256,10 +1258,10 @@ It can handle big files (tested up to 100mb). XML Entities, HTML entities, and D
     ; Use ESBuild because this package is used to build Typescript.
     (native-inputs (list esbuild))
     (arguments (list
-      #:modules '(
-        (guix build node-build-system)
-        (guix build utils)
-        (ice-9 match))
+      #:modules '((guix build node-build-system)
+                  (guix build json-utils)
+                  (guix build utils)
+                  (ice-9 match))
       #:tests? #f ; FIXME: Tests require 'jest'.
       #:phases
       #~(modify-phases %standard-phases
@@ -1390,11 +1392,12 @@ suitable for use with the @code{fs} module functions.")
       node-path-scurry))
     ; Use ESBuild instead of tshy because this is used to build Typescript.
     (native-inputs (list esbuild))
-    (arguments (list
-      #:modules '(
-        (guix build node-build-system)
-        (guix build utils)
-        (ice-9 match))
+    (arguments
+     (list
+      #:modules '((guix build node-build-system)
+                  (guix build json-utils)
+                  (guix build utils)
+                  (ice-9 match))
       #:tests? #f ; FIXME: Tests require 'c8' and 'tap'.
       #:phases
       #~(modify-phases %standard-phases
@@ -2064,10 +2067,10 @@ user-land JavaScript.")
     ; Use ESBuild instead of tshy because this is used to build Typescript.
     (native-inputs (list esbuild))
     (arguments (list
-      #:modules '(
-        (guix build node-build-system)
-        (guix build utils)
-        (ice-9 match))
+      #:modules '((guix build node-build-system)
+                  (guix build json-utils)
+                  (guix build utils)
+                  (ice-9 match))
       #:tests? #f ; FIXME: Tests require 'tap'.
       #:phases #~(modify-phases %standard-phases
         (add-before 'patch-dependencies 'delete-dependencies
@@ -2212,10 +2215,10 @@ JavaScript.")
     ; Use ESBuild because this is used to build Typescript.
     (native-inputs (list esbuild))
     (arguments (list
-      #:modules '(
-        (guix build node-build-system)
-        (guix build utils)
-        (ice-9 match))
+      #:modules '((guix build node-build-system)
+                  (guix build json-utils)
+                  (guix build utils)
+                  (ice-9 match))
       #:tests? #f ; FIXME: Tests require 'tap'.
       #:phases #~(modify-phases %standard-phases
         (add-before 'patch-dependencies 'modify-package
@@ -2351,6 +2354,7 @@ This is not a through or through2 stream. It doesn't transform the data, it just
      (list
       #:tests? #f ; FIXME: Tests require 'tap'.
       #:modules '((guix build node-build-system)
+                  (guix build json-utils)
                   (guix build utils)
                   (ice-9 match)
                   (srfi srfi-26))
@@ -2686,10 +2690,10 @@ particular cross-platform spellings of the PATH environment variable key.")
     ; Use ESBuild because this is used to build Typescript.
     (native-inputs (list esbuild))
     (arguments (list
-      #:modules '(
-        (guix build node-build-system)
-        (guix build utils)
-        (ice-9 match))
+      #:modules '((guix build node-build-system)
+                  (guix build json-utils)
+                  (guix build utils)
+                  (ice-9 match))
       #:tests? #f ; FIXME: Tests require 'tap'.
       #:phases #~(modify-phases %standard-phases
         (add-before 'patch-dependencies 'delete-dependencies
@@ -3620,6 +3624,7 @@ it to make a new binding for a different platform or underling technology.")))
        ((guix build node-build-system)
         (srfi srfi-1)
         (ice-9 match)
+        (guix build json-utils)
         (guix build utils))
        #:phases
        (modify-phases %standard-phases
@@ -3851,11 +3856,11 @@ connection.")))
     (build-system node-build-system)
     (native-inputs (list esbuild))
     (arguments (list
-      #:modules '(
-        (guix build node-build-system)
-        (guix build utils)
-        (ice-9 match)
-        (json))
+      #:modules '((guix build node-build-system)
+                  (guix build json-utils)
+                  (guix build utils)
+                  (ice-9 match)
+                  (json))
       #:phases #~(modify-phases %standard-phases
         (add-before 'patch-dependencies 'modify-package (lambda _
           (modify-json
@@ -3952,6 +3957,7 @@ connection.")))
        ((guix build node-build-system)
         (srfi srfi-1)
         (ice-9 match)
+        (guix build json-utils)
         (guix build utils))
        #:tests? #f ; FIXME: tests depend on node-mocha
        #:phases
