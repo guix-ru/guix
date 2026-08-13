a725be5859927ec58beef1d9af8e21be06685a88
Liliana Marie Prikler <liliana.prikler@gmail.com>
2026-08-13 13:28:49 +0200

    gnu: Move prettypst to typst.scm.
    * gnu/packages/rust-apps.scm (prettypst): Move from here…
    * gnu/packages/typst.scm (prettypst): … to here.

--- gnu/packages/typst.scm
+++ gnu/packages/typst.scm
@@ -30,6 +30,29 @@
   #:use-module (gnu packages rust-crates)
   #:use-module (gnu packages tls))
 
+(define-public prettypst
+  (package
+    (name "prettypst")
+    (version "2.0.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/antonWetzel/prettypst")
+             (commit version)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0727anhd2wl967m8k5z3bxb37h45nsjbbjz8akjn3mq96cfbfsbw"))))
+    (build-system cargo-build-system)
+    (arguments (list #:install-source? #f))
+    (inputs (cargo-inputs 'prettypst))
+    (home-page "https://github.com/antonWetzel/prettypst")
+    (synopsis "Configurable formatter for Typst")
+    (description
+     "Prettypst is a configurable source file formatter for the Typst
+typesetting system.")
+    (license license:expat)))
+
 (define-public typst
   (package
     (name "typst")
