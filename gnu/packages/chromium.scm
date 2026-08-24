e3909032064092384f76b610265655058904e834
André Batista <nandre@riseup.net>
2026-08-24 13:52:04 -0300

    gnu: ungoogled-chromium: Update to 151.0.7922.173 [security-fixes].
    Fixes CVEs: 2026-76017, 2026-76018, 2026-76019, 2026-76020, 2026-76021,
    2026-76022, 2026-76023, 2026-76033, 2026-76034, 2026-76035, 2026-76036,
    2026-76037, 2026-76038, 2026-76039, 2026-76040, 2026-76041, 2026-76042,
    2026-76043, 2026-76044, 2026-76045, 2026-76046 and 2026-76047.
    
    * gnu/packages/chromium.scm (%chromium-version): Update to 151.0.7922.173.
    (%debian-revision): Set to 'debian/151.0.7922.169-1', as debian has removed
    some llvm-19 patches that we still need until our libcxx is updated.
    (%ungoogled-origin, %debian-origin): Update hashes.
    
    References:
    https://chromereleases.googleblog.com/2026/08/stable-channel-update-for-desktop_0826575033.html
    https://chromereleases.googleblog.com/2026/08/stable-channel-update-for-desktop_0404570826.html
    Signed-off-by: Andreas Enge <andreas@enge.fr>

--- gnu/packages/chromium.scm
+++ gnu/packages/chromium.scm
@@ -406,9 +406,9 @@
   ;; run the Blink performance tests, just remove everything to save ~70MiB.
   '("third_party/blink/perf_tests"))
 
-(define %chromium-version "151.0.7922.137")
+(define %chromium-version "151.0.7922.173")
 (define %ungoogled-revision (string-append %chromium-version "-1"))
-(define %debian-revision (string-append "debian/" %ungoogled-revision))
+(define %debian-revision "debian/151.0.7922.169-1")
 
 (define %ungoogled-origin
   (origin
@@ -418,7 +418,7 @@
     (file-name (git-file-name "ungoogled-chromium" %ungoogled-revision))
     (sha256
      (base32
-      "03kia9fj40hlvvnz1v1gbzbhicfh5zj2f0am7yv3va4mmd3232a0"))))
+      "17m9sngn3ppb18mzj226l03839fg8czjs71kdvbs1lzci7mskdcv"))))
 
 (define %debian-origin
   (origin
@@ -431,7 +431,7 @@
                                 ((_ version) version))))
     (sha256
      (base32
-      "07ja5wk9p3vcak8czjn70cqg0x7pkwkm2yk9z9fz6925mipnl6yg"))))
+      "0lhp1l2x9p5v88l1cqqdki0zldszzkdcfl412makpcm0j92j0fwj"))))
 
 (define (origin-file origin file)
   (computed-file
@@ -629,7 +629,7 @@
                                   %chromium-version "-lite.tar.xz"))
               (sha256
                (base32
-                "0ignafc5yhhiknqi6wcw8s4wj1fs70jdcb4bcbv4b4f9q103989z"))
+                "1w2mlx2z59d31ndn8g7s8hc5lch9nlvcv6sf62kclsj76jrf5xc7"))
               (modules '((guix build utils)))
               (snippet (force ungoogled-chromium-snippet))))
     (build-system gnu-build-system)
