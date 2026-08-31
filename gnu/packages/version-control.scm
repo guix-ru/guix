646ced7d00bd22cc60d117f114f2be795f3ade29
Nguyễn Gia Phong <cnx@loang.net>
2026-08-31 10:10:20 +0900

    gnu: libgit2-1.9: Ungraft.
    * gnu/packages/version-control.scm (libgit2-1.9): Update to 1.9.7.
      [replacement]: Unset.  [source]<snippet>: Update list of bundles.
      (libgit2-1.9.7): Delete variable.
    * gnu/packages/rust-apps.scm (radicle)[inputs]:
      Replace libgit2-1.9.7 with libgit2-1.9.
    
    References: 98d646431e4f ("gnu: libgit2-1.9: Graft to 1.9.7.")

--- gnu/packages/version-control.scm
+++ gnu/packages/version-control.scm
@@ -1421,7 +1421,6 @@ other git-like projects such as @code{libgit2}.")
   (package
     (name "libgit2")
     (version "1.9.4")
-    (replacement libgit2-1.9.7)
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -1429,8 +1428,7 @@ other git-like projects such as @code{libgit2}.")
                     (commit (string-append "v" version))))
               (file-name (git-file-name "libgit2" version))
               (sha256
-               (base32
-                "07gfycsmq3hdaic3x3ldgnry3gnam1yxsy8q5f94s52xgb7j59b4"))
+               (base32 "0jhm723bc9v18yanlqvmwn9kpmnvbcavnlhd4jcw0n082d7jl54h"))
               (patches
                (search-patches "libgit2-proxy-reconnection.patch"
                                "libgit2-path-max.patch"))
@@ -1441,7 +1439,7 @@ other git-like projects such as @code{libgit2}.")
                              '("deps/chromium-zlib"
                                "deps/llhttp"
                                "deps/ntlmclient"
-                               "deps/pcre"
+                               "deps/pcre2"
                                "deps/winhttp"
                                "deps/zlib"))))))
     (build-system cmake-build-system)
@@ -1486,27 +1484,6 @@ provided as a re-entrant linkable library with a solid API, allowing you to
 write native speed custom Git applications in any language with bindings.")
     ;; GPLv2 with linking exception
     (license license:gpl2)))
-
-(define-public libgit2-1.9.7
-  (package
-    (inherit libgit2-1.9)
-    (name "libgit2")
-    (version "1.9.7")
-    (source
-     (origin
-       (inherit (package-source libgit2-1.9))
-       (uri (git-reference
-             (url "https://github.com/libgit2/libgit2")
-             (commit (string-append "v" version))))
-       (file-name (git-file-name "libgit2" version))
-       (sha256 (base32 "0jhm723bc9v18yanlqvmwn9kpmnvbcavnlhd4jcw0n082d7jl54h"))
-       (snippet #~(for-each delete-file-recursively
-                    '("deps/chromium-zlib"
-                      "deps/llhttp"
-                      "deps/ntlmclient"
-                      "deps/pcre2"
-                      "deps/winhttp"
-                      "deps/zlib")))))))
 
 (define-public libgit2-1.9/pinned
   ;; This is a pinned version used as a dependency for 'rust-cargo-c'.
