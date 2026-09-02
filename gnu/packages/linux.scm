ee5fab48f3e5588ccefe22900a2a0a535ca8c527
Vagrant Cascadian <vagrant@debian.org>
2026-09-02 10:52:52 -0700

    gnu: linux-libre 7.1: Update to 7.1.13.
    * gnu/packages/linux.scm (linux-libre-7.1-version): Update to 7.1.13.
    (linux-libre-7.1-pristine-source): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -607,10 +607,7 @@ GEXP)."
                             (%upstream-linux-source version hash)
                             deblob-scripts-7.2)))
 
-;; The current "stable" kernels. That is, the most recently released major
-;; versions that are still supported upstream.
-
-(define-public linux-libre-7.1-version "7.1.10")
+(define-public linux-libre-7.1-version "7.1.13")
 (define-public linux-libre-7.1-gnu-revision "gnu")
 (define deblob-scripts-7.1
   (linux-libre-deblob-scripts
@@ -620,7 +617,7 @@ GEXP)."
    (base32 "0ab66phhdwhhhvhywiylhlsmqvw8ny4xyi7ap7xzzkqc7im8w1j9")))
 (define-public linux-libre-7.1-pristine-source
   (let ((version linux-libre-7.1-version)
-        (hash (base32 "16crmblk96nwjzpbb22fpcj0r4ky63f1njvliv4vxwq2g9lz9lk7")))
+        (hash (base32 "1mqshj3ldn0md1c0am35skznnq5vmz3fvrr0cqmwwp6bzpx9akb1")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-7.1)))
