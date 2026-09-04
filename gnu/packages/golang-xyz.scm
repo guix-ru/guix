b14610797bff497475892480c639737919b6bb0d
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-09-04 13:03:07 +0100

    gnu: go-github-com-tidwall-gjson: Update to 1.19.0.
    * gnu/packages/golang-xyz.scm (go-github-com-tidwall-gjson): Update to 1.19.0.

--- gnu/packages/golang-xyz.scm
+++ gnu/packages/golang-xyz.scm
@@ -31139,16 +31139,16 @@ Linux, and macOS.")
 (define-public go-github-com-tidwall-gjson
   (package
     (name "go-github-com-tidwall-gjson")
-    (version "1.18.0")
+    (version "1.19.0")
     (source
      (origin
        (method git-fetch)
        (uri (git-reference
-             (url "https://github.com/tidwall/gjson")
-             (commit (string-append "v" version))))
+              (url "https://github.com/tidwall/gjson")
+              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0hn38yicvp0fnccib4ir2i82572fcbpdad50lc7ryqxw7fla3vh8"))))
+        (base32 "0ca9mnd67wak78j6b6ar89dvjmmg8pys4kzrp9isbf3hzdlnljqz"))))
     (build-system go-build-system)
     (arguments
      (list
