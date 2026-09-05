a6483347c9b591e1b276161ff9d9c86a279094d4
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-09-05 15:54:42 +0100

    gnu: go-github-com-quic-go-webtransport-go: Update to 0.11.0.
    * gnu/packages/golang-web.scm (go-github-com-quic-go-webtransport-go): Update to 0.11.0.
    [descriptions]: Fix TexInfo formatting.

--- gnu/packages/golang-web.scm
+++ gnu/packages/golang-web.scm
@@ -19224,7 +19224,7 @@ protocol.")
 (define-public go-github-com-quic-go-webtransport-go
     (package
       (name "go-github-com-quic-go-webtransport-go")
-      (version "0.10.0")
+      (version "0.11.0")
       (source
        (origin
          (method git-fetch)
@@ -19233,7 +19233,7 @@ protocol.")
                (commit (string-append "v" version))))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "0np42zg6pkflp1hfnl4xbgk3bkfvhm210mbw9qzldwglxh7mql7i"))))
+          (base32 "0h1lyjjnkqs61hd8x1i79nir01yqipc575nzi47vljc3djd6ci6i"))))
       (build-system go-build-system)
       (arguments
        (list
@@ -19254,10 +19254,10 @@ protocol.")
       (synopsis "WebTransport implementation based on quic-go")
       (description
        "webtransport-go is an implementation of the @code{WebTransport}
-protocol, based on @@url{https://github.com/quic-go/quic-go,quic-go}.  It
+protocol, based on @url{https://github.com/quic-go/quic-go, quic-go}.  It
 currently implements
-@@url{https://www.ietf.org/archive/id/draft-ietf-webtrans-http3-02.html,draft-02}
-of the specification.")
+@url{https://www.ietf.org/archive/id/draft-ietf-webtrans-http3-02.html,
+draft-02} of the specification.")
       (license license:expat)))
 
 (define-public go-github-com-rabbitmq-amqp091-go
