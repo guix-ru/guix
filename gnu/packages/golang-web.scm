c889331be144c5e73c752e3ea64ef6a056caa8ad
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-09-08 18:49:35 +0100

    gnu: go-github-com-go-openapi-swag-conv: Update to 0.29.2.
    * gnu/packages/golang-web.scm (go-github-com-go-openapi-swag-conv): Update to 0.29.2.
    [native-inputs]: Remove go-github-com-stretchr-testify; add
    go-github-com-go-openapi-testify-v2.

--- gnu/packages/golang-web.scm
+++ gnu/packages/golang-web.scm
@@ -9093,7 +9093,7 @@ projects.")
 (define-public go-github-com-go-openapi-swag-conv
   (package
     (name "go-github-com-go-openapi-swag-conv")
-    (version "0.24.0")
+    (version "0.29.2")
     (source
      (origin
        (method git-fetch)
@@ -9102,7 +9102,7 @@ projects.")
               (commit (go-version->git-ref version #:subdir "conv"))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0p1k008zh6s1v55fi83v8ihldsvknf0wmwmc6i2ks0vw5gbwmaam"))
+        (base32 "1kk84irx6ifln01b5w1rzyrx4gmpzf6nkm190n6hhw7s4y28pq80"))
        (modules '((guix build utils)))
        (snippet
         #~(begin
@@ -9120,7 +9120,7 @@ projects.")
       #:unpack-path "github.com/go-openapi/swag"))
     (native-inputs
      (list go-github-com-go-openapi-swag-typeutils
-           go-github-com-stretchr-testify))
+           go-github-com-go-openapi-testify-v2))
     (home-page "https://github.com/go-openapi/swag")
     (synopsis "Type conversion utilities for go-openapi")
     (description
