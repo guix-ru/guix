c4a93210d0442619fed7c4054d15bb6e79954d34
Morgan Smith <Morgan.J.Smith@outlook.com>
2026-09-08 21:59:07 -0400

    gnu: emacs-org-cliplink: Fix build.
    * gnu/packages/emacs-xyz.scm(emacs-org-cliplink)[#:phases]:
    Add ‘fix-glob-function’.
    
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -49707,6 +49707,12 @@ should pop up.")
        (list
         #:phases
         #~(modify-phases %standard-phases
+            (add-after 'unpack 'fix-glob-function
+              (lambda _
+                (substitute* "org-cliplink.el"
+                  ;; eshell changed their glob function so use dired one instead
+                  (("\\(require 'em-glob\\)") "(require 'dired)")
+                  (("eshell-glob-regexp") "dired-glob-regexp"))))
             (add-after 'unpack 'patch-curl-executable
               (lambda* (#:key inputs #:allow-other-keys)
                 (substitute* "org-cliplink-transport.el"
