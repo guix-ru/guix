9aa4928251d257c24a5d9ae8d8d239ae92f0b0a9
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-09-10 14:09:59 +0100

    gnu: go-github-com-openprinting-goipp: Update to 1.2.0.
    * gnu/packages/golang-xyz.scm (go-github-com-openprinting-goipp): Update to 1.2.0.

--- gnu/packages/golang-xyz.scm
+++ gnu/packages/golang-xyz.scm
@@ -25940,7 +25940,7 @@ composable way such that accurate error can be reasoned about.")
 (define-public go-github-com-openprinting-goipp
   (package
     (name "go-github-com-openprinting-goipp")
-    (version "1.1.0")
+    (version "1.2.0")
     (source
      (origin
        (method git-fetch)
@@ -25949,7 +25949,7 @@ composable way such that accurate error can be reasoned about.")
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "1p05dk37l393byvjanvi3ipqcax320vf3qynlzazm7czzzlw448h"))))
+        (base32 "0s5psffy5n4lkg9abfmfwajmrlx0ncn5qvih8xjlabrj8w01qdih"))))
     (build-system go-build-system)
     (arguments
      (list
