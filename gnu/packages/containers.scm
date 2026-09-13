332a1ca610b5e48fdb9bb716f57c32d6a542276f
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-09-13 21:43:34 +0100

    gnu: go-github-com-rootless-containers-rootlesskit-v3: Update to 3.1.0.
    * gnu/packages/containers.scm (go-github-com-rootless-containers-rootlesskit-v3): Update to 3.1.0.
    [arguments] <test-flags>: Skip vet during tests.
    [propagated-inputs]: Remove go-github-com-masterminds-semver-v3,
    go-github-com-sirupsen-logrus, and go-github-com-urfave-cli-v2.
    [native-inputs]: Add go-github-com-masterminds-semver-v3,
    go-github-com-sirupsen-logrus, and go-github-com-urfave-cli-v2.

--- gnu/packages/containers.scm
+++ gnu/packages/containers.scm
@@ -782,7 +782,7 @@ specification}.")
 (define-public go-github-com-rootless-containers-rootlesskit-v3
   (package
     (name "go-github-com-rootless-containers-rootlesskit-v3")
-    (version "3.0.0")
+    (version "3.1.0")
     (source
      (origin
        (method git-fetch)
@@ -791,12 +791,17 @@ specification}.")
               (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0giw1whjpm64h8f1iamgym246rr3wl01w7zgw4lygrj7dqk3clmb"))))
+        (base32 "1nxx0ws8gq2s5rh037g4ixmdd9lsjybgm7yf4hn09r1kvyx1g8cj"))))
     (build-system go-build-system)
     (arguments
      (list
       #:skip-build? #t
-      #:import-path "github.com/rootless-containers/rootlesskit/v3"))
+      #:import-path "github.com/rootless-containers/rootlesskit/v3"
+      #:test-flags #~(list "-vet=off")))
+    (native-inputs
+     (list go-github-com-masterminds-semver-v3
+           go-github-com-sirupsen-logrus
+           go-github-com-urfave-cli-v2))
     (propagated-inputs
      (list go-github-com-containernetworking-plugins
            go-github-com-containers-gvisor-tap-vsock
@@ -804,12 +809,9 @@ specification}.")
            go-github-com-google-uuid
            go-github-com-gorilla-mux
            go-github-com-insomniacslk-dhcp
-           go-github-com-masterminds-semver-v3
            go-github-com-moby-sys-mountinfo
            go-github-com-moby-vpnkit
-           go-github-com-sirupsen-logrus
            go-github-com-songgao-water
-           go-github-com-urfave-cli-v2
            go-golang-org-x-sync
            go-golang-org-x-sys
            go-gotest-tools-v3))
