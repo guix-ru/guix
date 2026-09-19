76afd0c5a561f0a54c426bb5d35a53a5b8fee0c3
Zheng Junjie <z572@z572.online>
2026-09-19 16:49:54 +0800

    gnu: gcc-13: Update to 13.5.0.
    * gnu/packages/gcc.scm (gcc-13): Update to 13.5.0.
    [source]: Remove gcc-hurd-split-stack.patch.

--- gnu/packages/gcc.scm
+++ gnu/packages/gcc.scm
@@ -958,20 +958,19 @@ It also includes runtime support libraries for these languages.")
 (define-public gcc-13
   (package
     (inherit gcc-11)
-    (version "13.4.0")
+    (version "13.5.0")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnu/gcc/gcc-"
                                   version "/gcc-" version ".tar.xz"))
               (sha256
                (base32
-                "1xd8kjssyhq82y8a8yhcpy6mmjfbql1sr22makf8ymj0n3dyck4w"))
+                "0mcq768c0c1w2gsasbxh489fnw1hvasdb74s3zwi256hbq0z0ggc"))
               (patches (search-patches "gcc-12-strmov-store-file-names.patch"
                                        "gcc-5.0-libvtv-runpath.patch"
                                        "gcc-13-libsanitizer-no-crypt.patch"
                                        "gcc-libstdc++-newer-gcc.patch"
                                        "gcc-mangle-guix-store.patch"
-                                       "gcc-hurd-split-stack.patch"
                                        "gcc-13-fix-hurd-lib.patch"))
               (modules '((guix build utils)))
               (snippet gcc-canadian-cross-objdump-snippet)))
